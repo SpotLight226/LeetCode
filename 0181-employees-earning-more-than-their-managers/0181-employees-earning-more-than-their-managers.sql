376428c62fd2f72d524c32c1fda45e029ce110bc
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2023-05-25 18:15:48 +0900

    Time: 823 ms (35.52%), Space: 0B (100.00%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,5 +1,6 @@
-SELECT e1.name AS Employee
-FROM Employee e1
-    INNER JOIN Employee e2
-    ON e1.managerId = e2.id
-WHERE e1.salary > e2.salary
+SELECT e.name AS Employee
+FROM Employee AS e
+     INNER JOIN Employee AS manager
+     ON e.managerId = manager.id
+WHERE e.salary > manager.salary
+    
