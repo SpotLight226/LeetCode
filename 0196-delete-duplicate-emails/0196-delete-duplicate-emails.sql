652af7ca79be54ada2a9f885ca7cdfe108f22395
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-07-26 20:39:42 +0900

    Time: 998 ms (31.11%), Space: 0B (100.00%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,6 +1,5 @@
--- ID가 포함되지 않는 것만 빼고 삭제 (NOT IN)
--- EMAIL로 그룹화해서 최소 ID(MIN(ID))를 선택하는 CTE 에서 MIN_ID 를 조건으로 설정
-DELETE FROM PERSON
-WHERE ID NOT IN (SELECT MIN_ID FROM (SELECT MIN(ID) AS MIN_ID
-                                     FROM PERSON
-                                     GROUP BY EMAIL) AS TEMP );
+-- PERSON 테이블을 두번 참조하여 P1, P2를 만든다
+-- P1.EMAIL = P2.EMAIL 이 조건으로 같은 이메일을 가진 행을 찾는다
+-- P1.ID > P2.ID 같은 이메일을 가진 행 중에서 ID가 더 큰 행을 선택하고 P1 행을 삭제한다
+DELETE P1 FROM PERSON AS P1, PERSON AS P2
+WHERE P1.EMAIL = P2.EMAIL AND P1.ID > P2.ID
