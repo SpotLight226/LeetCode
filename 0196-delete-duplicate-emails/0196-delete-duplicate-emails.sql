4727b93689ea6c47da3446165873189322e6838a
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-07-26 20:33:26 +0900

    Time: 496 ms (94.84%), Space: 0B (100.00%) - LeetHub

---
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -0,0 +1,6 @@
+DELETE FROM PERSON
+-- ID가 포함되지 않는 것만 빼고 삭제 (NOT IN)
+-- EMAIL로 그룹화해서 최소 ID(MIN(ID))를 선택하는 CTE 에서 MIN_ID 를 조건으로 설정
+WHERE ID NOT IN (SELECT MIN_ID FROM (SELECT MIN(ID) AS MIN_ID
+                                     FROM PERSON
+                                     GROUP BY EMAIL) AS TEMP );
