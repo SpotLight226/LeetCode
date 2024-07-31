ab0252b60f5eab5af070e47e29c3712582f3744e
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-07-31 22:40:27 +0900

    Time: 1145 ms (88.31%), Space: 0B (100.00%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,4 +1,6 @@
 -- RED 라는 회사와 거래가 없는 세일즈맨을 선택
+-- 내가 한 것
+/*
 WITH OrderRed AS (
                    SELECT DISTINCT(O.sales_id) AS RedSaleId -- 몇개가 나오든 하나만 나오게 중복 제거
                    FROM Orders AS O
@@ -11,6 +13,19 @@ SELECT name
 FROM SalesPerson
 WHERE sales_id NOT IN (SELECT *
                        FROM OrderRed) -- OrderRed 에 포함 되지않은 사람만
+*/
 
+-- 3중 select 로 임시 테이블 없이
+SELECT name 
+FROM SalesPerson
+WHERE sales_id NOT IN ( -- Orders 테이블에 있는 sales_id 에 없는 사람만 SELECT 
+                        SELECT sales_id
+                        FROM Orders
+                        WHERE com_id in ( -- RED인 회사의 com_id 를 조건으로 Orders 테이블에서 sales_id를 SELECT
+                                          SELECT com_id
+                                          FROM Company
+                                          WHERE name = "RED"
+                                        ) 
+                       )
 
 
