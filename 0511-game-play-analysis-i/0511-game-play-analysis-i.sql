8b52c512e3e8865aa752ec99b9cad033cfe84fc3
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-08-03 22:31:24 +0900

    Time: 581 ms (24.28%), Space: 0B (100.00%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -1,4 +1,4 @@
 SELECT player_id,
-       MIN(event_date) AS first_login
+       MIN(event_date) AS first_login -- event_date에서 최소 값
 FROM Activity
-GROUP BY player_id
+GROUP BY player_id -- id로 그룹화
