5471501b8f7900301c154200f1aa69cc706d7fdb
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-08-05 21:08:16 +0900

    Time: 482 ms (50.83%), Space: 0B (100.00%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -1,6 +1,7 @@
 SELECT activity_date AS day,
        COUNT(DISTINCT(user_id)) AS active_users
 FROM Activity
+-- 2019-07-27를 포함하기 위해서 INTERVAL 29DAY를 DATE_SUB에 사용한다
 WHERE activity_date BETWEEN DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND '2019-07-27'
 GROUP BY activity_date
 
