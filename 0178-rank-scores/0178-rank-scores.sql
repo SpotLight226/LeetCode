f90c0260ae30ca78fd7a57c84f1a9d876e074e9a
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-08-16 21:13:22 +0900

    Time: 283 ms (70.22%), Space: 0B (100.00%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,3 +1,7 @@
 SELECT score,
+       -- DENSE_RANK() => 동일한 순위가 부여된 다음에도 순위가 건너뛰지 않는다
+       -- RANK() => 동일한 순위가 부여되면 다음에는 순위가 건너뛴다
+       -- 둘 다 OVER(정렬 기준) 을 같이 쓴다
+       -- ROW_NUMBER() => 데이터에 순차적인 번호를 부여 (OVER 같이 씀)
        DENSE_RANK() OVER(ORDER BY score DESC) AS 'rank'
 FROM Scores
