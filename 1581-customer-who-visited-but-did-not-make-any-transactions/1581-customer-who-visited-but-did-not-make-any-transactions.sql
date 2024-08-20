a02f3786ae9178c756fa4e444a8e9a0f7d4e96aa
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-08-20 22:36:18 +0900

    Time: 1234 ms (64.78%), Space: 0B (100.00%) - LeetHub

---
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,7 @@
+SELECT customer_id,
+       COUNT(customer_id) AS count_no_trans
+FROM Visits AS V
+LEFT JOIN Transactions AS T
+       ON V.visit_id = T.visit_id
+WHERE transaction_id IS NULL
+GROUP BY customer_id
