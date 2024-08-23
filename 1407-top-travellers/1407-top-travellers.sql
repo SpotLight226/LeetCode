865e8b7092d44108b8db14c3abb83898abb94928
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-08-23 22:37:00 +0900

    Time: 808 ms (50.97%), Space: 0B (100.00%) - LeetHub

--- 1407-top-travellers/1407-top-travellers.sql
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -1,4 +1,5 @@
--- 이동 거리 합 CTE
+/*
+-- 이동 거리 합 CTE를 이용한 방법 Runtime 863ms
 WITH Sum_dis AS (SELECT user_id,
                         SUM(distance) AS sum_distance
                  FROM Rides
@@ -10,3 +11,13 @@ FROM Users AS U
 LEFT JOIN Sum_dis AS S
        ON U.id = S.user_id
 ORDER BY S.sum_distance DESC, U.name -- 이동거리로 내림차순, 같다면 이름으로 정렬
+*/
+
+
+SELECT U.name,
+       COALESCE(SUM(R.distance), 0) travelled_distance
+FROM Users AS U 
+LEFT JOIN Rides AS R
+ON U.id = R.user_id
+GROUP BY U.id
+ORDER BY SUM(R.distance) DESC, U.name;
