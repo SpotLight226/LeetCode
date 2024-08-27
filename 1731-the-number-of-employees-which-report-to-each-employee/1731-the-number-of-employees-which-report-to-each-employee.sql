7f442e81b5667cf1f48fe78548b5bde6084f570d
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-08-27 21:09:10 +0900

    Time: 1151 ms (7.07%), Space: 0B (100.00%) - LeetHub

--- 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -1,13 +1,14 @@
+-- 보고자가 있는 사원들만 모은 CTE
 WITH Reporters AS (SELECT *
                    FROM Employees
-                   WHERE reports_to IS NOT NULL)
+                   WHERE reports_to IS NOT NULL) 
 
 SELECT E.employee_id,
        E.name,
-       COUNT(R.name) AS reports_count,
-       ROUND(AVG(R.age), 0) AS average_age 
+       COUNT(R.name) AS reports_count, -- 보고자 이름을 카운트
+       ROUND(AVG(R.age), 0) AS average_age  -- 나이 평균에 근접한 숫자로 반올림
 FROM Employees AS E 
-JOIN Reporters AS R
+JOIN Reporters AS R -- 보고자가 있는 사람 테이블과 JOIN해서 해당 사원에게 붙인다
   ON E.employee_id = R.reports_to
-GROUP BY E.employee_id
-ORDER BY E.employee_id
+GROUP BY E.employee_id -- 조인한 테이블을 사원 번호로 그룹화
+ORDER BY E.employee_id -- 사원 번호로 정렬 
