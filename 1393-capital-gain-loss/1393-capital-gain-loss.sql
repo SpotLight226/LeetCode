dac30078a927e4c279306deff9744499097afc68
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-08-28 21:50:16 +0900

    Time: 395 ms (98.92%), Space: 0B (100.00%) - LeetHub

--- 1393-capital-gain-loss/1393-capital-gain-loss.sql
+++ 1393-capital-gain-loss/1393-capital-gain-loss.sql
@@ -1,3 +1,5 @@
+/*
+-- 미리 산 주식, 판 주식의 합을 CTE로 만들어 계산하는 방식
 WITH Buy_stocks AS (SELECT stock_name,
                            SUM(price) AS buy_price
                     FROM Stocks
@@ -15,4 +17,15 @@ SELECT S.stock_name,
 FROM Sell_stocks AS S
 JOIN Buy_stocks AS B
   ON S.stock_name = B.stock_name
-  
+*/  
+
+-- CASE 문에서 합의 조건을 지정해서 합을 구하는 방식
+SELECT stock_name,
+       SUM(
+           CASE
+               WHEN operation = 'Buy' THEN -price
+               ELSE price
+           END
+          ) AS capital_gain_loss
+FROM Stocks
+GROUP BY stock_name
