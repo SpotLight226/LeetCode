ea120f6e5cebb13f237bd7d1b8a2defe860253d2
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-08-30 21:24:56 +0900

    Time: 655 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 3220-odd-and-even-transactions/3220-odd-and-even-transactions.sql
+++ 3220-odd-and-even-transactions/3220-odd-and-even-transactions.sql
@@ -1,4 +1,5 @@
 SELECT transaction_date,
+       -- SUM에 조건을 붙여서 CASE WHEN 조건 THEN 합할 컬럼 ELSE 조건에 안 맞으면 넣을 값 
        SUM(CASE
                WHEN (amount % 2) = 1 THEN amount
            ELSE 0 END) AS odd_sum,
