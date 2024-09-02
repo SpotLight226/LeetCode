5b692e27f8b4df8b0af7ccd999d3bb14c63bfc0f
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-09-02 20:59:07 +0900

    Time: 2267 ms (5.00%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,5 +1,5 @@
 SELECT MAX(salary) AS SecondHighestSalary 
 FROM Employee
-WHERE salary < (SELECT MAX(salary)
+WHERE salary < (SELECT MAX(salary) -- 최고 연봉을 조건으로 작은 것 중에서 max 인것
                 FROM Employee)
 
