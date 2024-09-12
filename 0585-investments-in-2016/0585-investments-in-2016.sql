7d41f32e3c27c604dd1c1527611c7af8b7a5234a
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-09-12 20:01:43 +0900

    Time: 3833 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -3,9 +3,9 @@ FROM Insurance
 WHERE tiv_2015 IN (SELECT tiv_2015
                    FROM Insurance
                    GROUP BY tiv_2015
-                   HAVING COUNT(*) > 1)
+                   HAVING COUNT(*) > 1) -- tiv_2015가 있으며
 AND (lat, lon) IN (SELECT lat,
                             lon
                    FROM Insurance
                    GROUP BY lat, lon
-                   HAVING COUNT(*) = 1)
+                   HAVING COUNT(*) = 1) -- lat, lon 이 고유해야 함
