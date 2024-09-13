4297c005ab973556147a9bdf07527800301f3cdc
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-09-13 22:40:35 +0900

    Time: 0 ms (100%), Space: 41.6 MB (36.15%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -4,12 +4,13 @@ class Solution {
             return null;
         }
 
-        String str = strs[0];
+        String str = strs[0]; // 첫 번째 문자열이 기준
 
         for (int i = 1; i < strs.length; i++) {
-            while (strs[i].indexOf(str) != 0) {
-                str = str.substring(0, str.length() - 1);
-                if (str.isEmpty()) {
+            // indexOf(str) 로 기준 문자열의 시작 지점이 0인지를 확인 하여 접두사인지 확인
+            while (strs[i].indexOf(str) != 0) { // 0이 아니라면 접두사가 아님
+                str = str.substring(0, str.length() - 1); // 기준 문자열에서 뒤의 한글자 제거
+                if (str.isEmpty()) { // 기준 문자열이 공백이되면 접두사가 없음
                     return "";
                 }
             }
