438f2bcbb0eaf0fdb38c47d43d8d9be4b42bff9c
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-09-21 21:38:11 +0900

    Time: 1642 ms (58.79%), Space: 0B (100%) - LeetHub

--- 2057-count-salary-categories/2057-count-salary-categories.sql
+++ 2057-count-salary-categories/2057-count-salary-categories.sql
@@ -1,5 +1,6 @@
 -- COUNT 에 IF 문을 넣어서 조건 맞을 때와 틀릴 때의 카운트를 설정할수 있다
 -- COUNT는 0도 카운트에 넣기 때문에 0으로 넣으면 안되고 NULL로 해야 한다
+-- 각기 다른 조건들로 카운트하기 위해서 각 조건 별로 COUNT를 해주는 SELECT 문을 UNOIN 한다
 SELECT 'Low Salary' AS category,
         COUNT(IF(income < 20000, 1, NULL)) AS accounts_count
 FROM Accounts
