e053abc5e64c19d58853769c98f1bfab67924999
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-09-26 22:10:17 +0900

    Time: 0 ms (100%), Space: 43.5 MB (6.86%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -5,14 +5,15 @@ class Solution {
         int r = nums.length - 1;
 
         while (l <= r) {
-            int mid = (l + r) / 2;
-
+            int mid = (l + r) / 2; // 중간 값
+            // 중간 인덱스의 값을 기준으로 l(왼쪽), r(오른쪽) 의 값을 조정한다
+            // target을 찾았으면 리턴
             if (nums[mid] == target) {
                 return mid;
-            } else if (nums[mid] < target) {
+            } else if (nums[mid] < target) { // mid인덱스의 값이 target보다 작다면 l을 mid + 1로
                 l = mid + 1;
             } else {
-                r = mid - 1;
+                r = mid - 1; // mid인덱스 값이 target보다 크다면 r을 mid - 1로
             }
         }
 
