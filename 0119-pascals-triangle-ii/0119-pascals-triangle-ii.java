3b41bb763c60c0aef7edd8dc4d9cb13e3779beaf
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-10-03 20:41:54 +0900

    Time: 1 ms (69.15%), Space: 41.4 MB (13.03%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -1,24 +1,26 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-         List<List<Integer>> pascal = new ArrayList<>();
+        // 파스칼 삼각형 만들기
+        List<List<Integer>> pascal = new ArrayList<>();
         List<Integer> row = new ArrayList<>();
 
-        row.add(1);
-        pascal.add(row);
+        row.add(1); // 첫 행은 1부터 시작
+        pascal.add(row); // 첫 행 넣기
 
         for (int i = 1; i < rowIndex + 1; i++) {
-            row = new ArrayList<>();
-            row.add(1);
+            row = new ArrayList<>(); // 행 초기화
+            row.add(1); // 모든 행 시작은 1
 
+            // 행이 3보다 크다면 이전 행의 각각의 요소를 더해 n을 만들고 행에 넣는다
             for (int j = 1; j < i; j++) {
                 int n = pascal.get(i - 1).get(j - 1) + pascal.get(i - 1).get(j);
                 row.add(n);
             }
 
-            row.add(1);
-            pascal.add(row);
+            row.add(1); // 모든 행의 끝은 1
+            pascal.add(row); // 행 넣기
         }
 
-        return pascal.get(rowIndex);
+        return pascal.get(rowIndex); // 해당 행 리턴
     }
 }
