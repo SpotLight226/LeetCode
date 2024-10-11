64754ff71bf00e0ce59a1b5fb8100a2d48d3afe6
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-10-11 20:58:12 +0900

    Time: 1 ms (99.49%), Space: 44.9 MB (78.14%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -3,13 +3,13 @@ class Solution {
         List<String> list = new ArrayList<>();
         
         for(int i = 1; i <= n; i++){
-            if(i % 15 == 0){
+            if(i % 15 == 0){ // i 가 3 과 5로 나누어 지면 즉 15로 나누었을 때 나머지가 0
                 list.add("FizzBuzz");
-            }else if(i % 3 == 0){
-                list.add("Fizz");
-            }else if( i % 5 == 0){
+            }else if(i % 3 == 0){ // 3으로 나누어지면 Fizz
+                list.add("Fizz"); 
+            }else if( i % 5 == 0){ // 5로 나누어지면 Buzz
                 list.add("Buzz");
-            }else{
+            }else{ // 3개의 조건에 해당 안 하면 그냥 숫자
                 list.add(Integer.toString(i));
             }
         }
