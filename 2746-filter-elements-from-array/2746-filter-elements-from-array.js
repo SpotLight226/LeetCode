1c216cad2e577b609b1c60829a47a5f9f7829482
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-10-15 22:56:41 +0900

    Time: 47 ms (85.86%), Space: 49.3 MB (13.85%) - LeetHub

--- 2746-filter-elements-from-array/2746-filter-elements-from-array.js
+++ 2746-filter-elements-from-array/2746-filter-elements-from-array.js
@@ -4,12 +4,12 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    let temp = [];
-    for(let i = 0; i < arr.length; i++){
-        if(fn(arr[i], i)){
+    let temp = []; // 임시 배열
+    for(let i = 0; i < arr.length; i++){ // 자바 스크립트에서는 자바의 반복문과 동일하지만 변수 형이 let(중복 X)
+        if(fn(arr[i], i)){ // fn에 arr의 요소를 넣었을 때 값이 있다면, i를 인덱스로 임시 배열 temp에 넣기
             temp.push(arr[i]);
         }
     }
     
-    return temp
+    return temp // temp 리턴
 };
