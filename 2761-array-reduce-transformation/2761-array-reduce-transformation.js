92ab2bcca4512c1c5eae3483d247884e3c41c781
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-10-18 19:52:41 +0900

    Time: 40 ms (98.78%), Space: 49 MB (82.74%) - LeetHub

--- 2761-array-reduce-transformation/2761-array-reduce-transformation.js
+++ 2761-array-reduce-transformation/2761-array-reduce-transformation.js
@@ -5,10 +5,14 @@
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
-    let val = init;
-    // 반복문으로 최종 리턴 val 와 nums 배열의 요소를 넘겨 계산
-    for(let i = 0; i < nums.length; i++){
-        val = fn(val, nums[i]);
-    }
-    return val;
+    // let val = init;
+    // // 반복문으로 최종 리턴 val 와 nums 배열의 요소를 넘겨 계산
+    // for(let i = 0; i < nums.length; i++){
+    //     val = fn(val, nums[i]);
+    // }
+    // return val;
+
+    // forEach 로 nums의 요소들을 e로 반복해서 init에 fn을 거친 값을 대입
+    nums.forEach( e => init = fn(init, e))
+    return init;
 };
