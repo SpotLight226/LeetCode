7c06b5af74c2ed9ecc993c224eefcdc0548ca1ef
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-10-30 22:58:10 +0900

    Time: 1791 ms (22.09%), Space: 0B (100%) - LeetHub

--- 1724-customer-who-visited-but-did-not-make-any-transactions/1724-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/1724-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,4 +1,5 @@
 /*
+-- 조인해서 방문은 했지만 거래가 없는 고객 찾기(transaction_id가 null인 것) 그래서 래프트 조인
 SELECT customer_id,
        COUNT(customer_id) AS count_no_trans
 FROM Visits AS V
@@ -8,6 +9,7 @@ WHERE transaction_id IS NULL
 GROUP BY customer_id
 */
 -- JOIN 사용 안하고 윈도우 함수 이용해 풀이
+-- 거래 테이블에서 visit_id가 없는 것만 개수 세기
 SELECT customer_id,
        COUNT(visit_id) AS count_no_trans
 FROM Visits
