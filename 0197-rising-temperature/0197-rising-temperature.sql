0dfeb7d092e715a3b49f1c0bb1ac841dc27bd62e
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-10-31 20:22:11 +0900

    Time: 558 ms (37.96%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,11 +1,6 @@
-SELECT w.id
-FROM Weather
-     JOIN Weather w
-     ON DATEDIFF(Weather.recordDate, w.recordDate) = -1
-WHERE Weather.temperature < w.temperature
-
-
-
-
-
+SELECT today.id
+FROM Weather yesterday
+CROSS JOIN Weather today -- 카데시안 곱을 이용해 모든 각 행에 다른 행들을 붙인다
+WHERE DATEDIFF(today.recordDate, yesterday.recordDate) = 1 -- DATEDIFF(뺄 날짜, 원 날짜)
+  AND today.temperature > yesterday.temperature -- 오늘의 온도가 어제의 온도보다 높은 것
 
