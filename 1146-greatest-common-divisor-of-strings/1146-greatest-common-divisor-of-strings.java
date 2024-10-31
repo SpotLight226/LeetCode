2a5dd38618ad6b814e6529475c28ea1fa71b9e6e
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-10-31 20:16:51 +0900

    Time: 0 ms (100%), Space: 42.1 MB (63.14%) - LeetHub

---
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+
+        int n1 = str1.length();
+        int n2 = str2.length();
+
+        while (n2 > 0) {
+            int temp = n2;
+            n2 = n1 % n2;
+            n1 = temp;
+        }
+
+        return str1.substring(0, n1);
+    }
+}
