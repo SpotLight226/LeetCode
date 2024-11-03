37bd23ddc4601a9b92f8dd265b516a75c229634f
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-11-03 22:31:15 +0900

    Time: 724 ms (57.38%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -1,5 +1,5 @@
 /*
--- 내가 한 것
+-- 내가 한 것 88비트
 -- 조건을 많이 적용해야해서 좀 헷갈렸음
 -- 만약 Confirmations테이블에서 user_id가 0개라면 0, 아니라면 action이 컨펌인 것만 1로 해서 더한 것(아니라면 0)을
 -- Confirmations테이블의 user_id 개수로 나눈 것을 2번째 자리까지 반올림한 것을 rate로 나타낸다
@@ -12,7 +12,7 @@ GROUP BY S.user_id
 ORDER BY confirmation_rate
 */
 
--- 대박!
+-- 대박! 96비트
 -- AVG에 IF문을 적용해서 action이 컨펌인 것만 1, 아닌것은 0으로 해서 각 아이디의 개수에서 평균을 구한다
 SELECT S.user_id,
        ROUND(AVG(IF(C.action = 'confirmed', 1, 0)), 2) AS confirmation_rate
