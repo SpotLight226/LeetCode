3c4a539ba5c41abbd6a31f0624d11b326bfc415c
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-11-03 22:27:31 +0900

    Time: 2317 ms (5%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,6 +1,6 @@
 SELECT E1.name
 FROM Employee AS E1
 JOIN Employee AS E2
-  ON E1.id = E2.managerId
+  ON E1.id = E2.managerId -- Employee 테이블 서로 조인하여, 각 id 당 매니저로서 할당된 개수를 센다
 GROUP BY E2.managerId
 HAVING COUNT(E2.managerId) >= 5
