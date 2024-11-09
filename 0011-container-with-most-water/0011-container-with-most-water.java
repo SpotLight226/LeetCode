d9d59c41de619dc7546dbbcf521329441a9538a6
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-11-09 22:11:19 +0900

    Time: 3 ms (96.01%), Space: 57.5 MB (78.37%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,25 +1,27 @@
 class Solution {
     public int maxArea(int[] height) {
-        int l = 0;
-        int r = height.length - 1;
+        int l = 0; // 왼쪽 포인터
+        int r = height.length - 1; // 오른쪽 포인터
 
-        int max = Integer.MIN_VALUE;
+        int max = Integer.MIN_VALUE; // 최대 값
 
-        while (l < r) {
-            int lHeight = height[l];
-            int rHeight = height[r];
+        // 포인터를 이동하며 크기 확인
+        while (l < r) { 
+            int lHeight = height[l]; // 왼쪽 길이
+            int rHeight = height[r]; // 오른쪽 길이
 
             int temp = 0;
-
+            // 왼쪽 길이가 오른쪽 길이보다 작거나 같다면 왼쪽 기준
             if (lHeight <= rHeight) {
-                temp = lHeight * (r - l);
+                temp = lHeight * (r - l); // 왼쪽 길이 * (오른쪽 포인터 - 왼쪽 포인터) // 가로 길이
+                // 최대값 갱신
                 if (temp > max) {
                     max = temp;
                 }
-                l++;
+                l++; // 왼쪽 포인터 옮겨서 다음 크기 확인
 
-            } else {
-                temp = rHeight * (r - l);
+            } else { // 오른쪽 길이 기준
+                temp = rHeight * (r - l); // 오른쪽 길이 * (오른쪽 포인터 - 왼쪽 포인터)
                 if (temp > max) {
                     max = temp;
                 }
