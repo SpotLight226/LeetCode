9361fab33f23222c89c0c621ea9bf627391293a9
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-11-11 23:56:23 +0900

    Time: 2 ms (100%), Space: 56.1 MB (52.86%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,20 +1,21 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-       int sum = 0;
-
-       for(int i = 0; i < k; i ++) {
-           sum += nums[i];
-       }
+        int sum = 0;
        
-       int max = sum;
+        // 일단 0 부터 k까지의 합을 구함
+        for(int i = 0; i < k; i ++) {
+            sum += nums[i];
+        }
 
-       for(int i = k; i < nums.length; i ++) {
+        int max = sum; // 최대 값을 설정
 
-           sum += nums[i] - nums[i - k];
+      
+        // (i - k) -> 0부터 시작) 을 빼고 nums[k]를 더해 합을 조정하며, 최대 값과 현재 합 값을 비교해 최대 값을 설정
+        for(int i = k; i < nums.length; i ++) {
+            sum += nums[i] - nums[i - k];
+            max = Math.max(max, sum);
+        }
 
-           max = Math.max(max, sum);
-       }
-
-       return (double)max / k;
+        return (double)max / k; // 평균을 리턴하기 위해서 max / k 
     }
 }
