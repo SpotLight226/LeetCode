3cf5d4f6a109c39e81cc2219817249d50650830d
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-11-14 20:54:23 +0900

    Time: 1031 ms (6.37%), Space: 0B (100%) - LeetHub

--- 2495-number-of-unique-subjects-taught-by-each-teacher/2495-number-of-unique-subjects-taught-by-each-teacher.sql
+++ 2495-number-of-unique-subjects-taught-by-each-teacher/2495-number-of-unique-subjects-taught-by-each-teacher.sql
@@ -1,4 +1,4 @@
 SELECT teacher_id,
-       COUNT(DISTINCT (subject_id)) AS cnt -- subject_id의 중복을 제거한 것을 count
+       COUNT(DISTINCT subject_id) AS cnt -- subject_id의 중복을 제거한 것을 count
 FROM Teacher
 GROUP BY teacher_id
