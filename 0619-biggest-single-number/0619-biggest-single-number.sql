d301e18ed058a5adb20ef626a87bac2ce83f0aaf
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-11-19 21:24:30 +0900

    Time: 449 ms (52.61%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,8 +1,8 @@
-
-SELECT COALESCE (
-      (SELECT num
-       FROM MyNumbers
-       GROUP BY num
-       HAVING COUNT(*) = 1
-       ORDER BY num DESC
-       LIMIT 1), NULL) AS num
+-- COALESCE => 서브 쿼리에서 결과를 반환하지 않으면 설정값을 반환하게 만든다
+-- SELECT COALESCE((서브 쿼리), 설정값) AS 컬럼 헤더
+SELECT COALESCE ((SELECT num
+                  FROM MyNumbers
+                  GROUP BY num
+                  HAVING COUNT(*) = 1
+                  ORDER BY num DESC
+                  LIMIT 1), NULL) AS num
