87121d8f245372ad4c527456b5770235cfd166b6
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-11-19 21:28:19 +0900

    Time: 60 ms (12.65%), Space: 49 MB (29.4%) - LeetHub

--- 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
@@ -4,11 +4,19 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
+    /*
     let transArr = []; // 변형된 숫자를 저장할 배열
     
     // arr의 배열을 순회하며, fn에 arr[i]와 사용할 i를 넣는다
     for(let i = 0; i < arr.length; i++){
         transArr[i] = fn(arr[i], i);
     }
+    return transArr;
+    */
+    
+    const transArr = [];
+    arr.forEach((element, index) => {
+        transArr[index] = fn(element, index);
+    })
     return transArr;
 };
