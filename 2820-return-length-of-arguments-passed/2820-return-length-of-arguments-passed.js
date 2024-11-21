a8d300543d588a54f75d30517995a65582a44fc9
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-11-21 21:38:40 +0900

    Time: 50 ms (66.07%), Space: 49.2 MB (6.03%) - LeetHub

--- 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.js
+++ 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.js
@@ -2,9 +2,15 @@
  * @param {...(null|boolean|number|string|Array|Object)} args
  * @return {number}
  */
+ /*
 var argumentsLength = function(...args) {
     return args.length;
 };
+*/
+
+function argumentsLength() {
+    return arguments.length;
+}
 
 /**
  * argumentsLength(1, 2, 3); // 3
