6f39495a3df14def94742cbaeb16596c04b5e7da
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-12-04 21:21:57 +0900

    Time: 298 ms (85.43%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,13 +1,15 @@
--- 친구는 
-WITH Temp_cte AS (SELECT requester_id AS id
-                  FROM RequestAccepted
-                  UNION ALL
-                  SELECT accepter_id AS id
-                  FROM RequestAccepted)
+-- RequestAccepted는 요청에 대해서 승낙한 id를 나타낸다.
+-- 즉, 리퀘스터든 엑셉트든 결국 친구가 되었다는 거임
+-- 친구 요청을 승낙한 사람과, 요청한 사람을 각각 나눈 CTE를 만들고 id의 각 개수를 센다 
+WITH IdCte AS (SELECT requester_id AS id
+               FROM RequestAccepted
+               UNION ALL
+               SELECT accepter_id AS id
+               FROM RequestAccepted)
 
 SELECT id,
        COUNT(*) AS num
-FROM Temp_cte
+FROM IdCte
 GROUP BY id
-ORDER BY COUNT(*) DESC -- 가장 많은 수를 세기 위해서 내림차순 정렬
-LIMIT 1
+ORDER BY COUNT(*) DESC -- 가장 많은 사람을 고르기 위해 내림차순
+LIMIT 1 -- 가장 위의 1개만 자르기
