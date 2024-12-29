df02b3adad787f98b8fbc3b8c1d529c37cd6d3ee
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-12-29 21:46:49 +0900

    Time: 516 ms (14.99%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -2,5 +2,5 @@ SELECT name,
        population,
        area
 FROM World
-WHERE population >= 25000000
-   OR area >= 3000000
+WHERE population >= 25000000 -- 인구가 250만 이상
+   OR area >= 3000000 -- 면적이 300만 이상
