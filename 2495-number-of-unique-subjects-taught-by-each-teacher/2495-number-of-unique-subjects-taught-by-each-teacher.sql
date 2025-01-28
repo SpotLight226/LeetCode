2f2596dd53cd6a296e57738095ebddc9ac96015f
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-01-28 21:54:49 +0900

    Time: 1190 ms (16.59%), Space: 0B (100%) - LeetHub

--- 2495-number-of-unique-subjects-taught-by-each-teacher/2495-number-of-unique-subjects-taught-by-each-teacher.sql
+++ 2495-number-of-unique-subjects-taught-by-each-teacher/2495-number-of-unique-subjects-taught-by-each-teacher.sql
@@ -1,4 +1,5 @@
+# Write your MySQL query statement below
 SELECT teacher_id,
-       COUNT(DISTINCT subject_id) AS cnt -- subject_id의 중복을 제거한 것을 count
+       COUNT(DISTINCT(subject_id)) AS cnt -- 각 선생당 중복을 제외한 과목의 수
 FROM Teacher
-GROUP BY teacher_id
+GROUP BY teacher_id -- 선생별로 묶기위한 group by
