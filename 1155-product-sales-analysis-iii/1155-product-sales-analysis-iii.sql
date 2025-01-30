65e2a1bdc6118edf7e6681fef6d2710865e281fc
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-01-30 23:59:02 +0900

    Time: 3071 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -1,11 +1,10 @@
-
+# Write your MySQL query statement below
 SELECT product_id,
        year AS first_year,
        quantity,
-       price
-FROM Sales
+       price 
+FROM sales
 WHERE (product_id, year) IN (SELECT product_id,
-                                    MIN(year) AS year
-                             FROM Sales
+                                     min(year)
+                             FROM sales
                              GROUP BY product_id)
--- 조건으로 각 product_id 에 대한 가장 작은 year을 넣는다
