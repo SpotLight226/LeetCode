4d68942e16378d86ae0ce1d001afc34546b91b8f
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-01-30 23:59:40 +0900

    Time: 572 ms (43.93%), Space: 0B (100%) - LeetHub

--- 0596-classes-more-than-5-students/0596-classes-more-than-5-students.sql
+++ 0596-classes-more-than-5-students/0596-classes-more-than-5-students.sql
@@ -1,4 +1,5 @@
+# Write your MySQL query statement below
 SELECT class
-FROM COURSES
-GROUP BY class -- 수업 별로 그룹화
-HAVING COUNT(student) >= 5 -- 그룹화한 수업의 개수가 5 이상인 것만
+FROM Courses
+GROUP BY class
+HAVING COUNT(student) >= 5;
