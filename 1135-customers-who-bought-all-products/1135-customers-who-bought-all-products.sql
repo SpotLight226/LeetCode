8e3b6f63ca2c113cf97358d4ab4795810bfae3e7
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-02-04 22:52:18 +0900

    Time: 507 ms (99%), Space: 0B (100%) - LeetHub

--- 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
@@ -1,8 +1,5 @@
 SELECT customer_id
-FROM Customer AS C
-JOIN Product AS P
-  ON C.product_key = P.product_key -- 제품 키로 조인
-GROUP BY customer_id -- 손님 테이블에서 ID로 그룹화
-HAVING COUNT(DISTINCT(C.product_key)) = (SELECT COUNT(product_key)
-                                         FROM Product)
--- HAVING 조건에 손님 테이블에서 중복을 제외한 제품 키의 개수가 제품 테이블에서 제품 키의 개수가 같은 것만 
+FROM Customer
+GROUP BY customer_id
+HAVING COUNT(DISTINCT(product_key)) = (SELECT COUNT(*) FROM Product);
+-- 손님 id로 그룹화해서 각 손님당 제품 키의 개수가 Product 테이블의 총 제품키 개수와 같은 것만
