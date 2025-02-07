887a2c60234e705f32a6ba5528097233dcfbd0d7
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-02-07 19:53:30 +0900

    Time: 788 ms (74.34%), Space: 0B (100%) - LeetHub

--- 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -1,8 +1,9 @@
+# Write your MySQL query statement below
 SELECT employee_id,
        department_id
 FROM Employee
-WHERE primary_flag = 'Y' -- 조건 1 => 기본 부서가 Y인 것
-OR employee_id IN (SELECT employee_id -- 조건 2 => id가 GROUP BY 했을 때 COUNT가 1인 것만
-                   FROM Employee
-                   GROUP BY employee_id
-                   HAVING COUNT(*) = 1 )
+WHERE primary_flag = 'Y' -- 주 부서 이거나
+   OR employee_id in (SELECT employee_id
+                      FROM Employee
+                      GROUP BY employee_id
+                      HAVING COUNT(employee_id) = 1) -- id가 하나 뿐인 서브쿼리에 속해있는 것만
