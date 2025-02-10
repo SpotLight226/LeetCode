f0839a66f34707d04a3294ae0f822f25f3f1eade
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-02-10 18:56:21 +0900

    Time: 357 ms (85.59%), Space: 0B (100%) - LeetHub

--- 0610-triangle-judgement/0610-triangle-judgement.sql
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -1,9 +1,7 @@
-SELECT X,
-       Y,
-       Z,
-       CASE
-           WHEN X + Y > Z AND X + Z > Y AND Y + Z > X
-           THEN 'Yes'
+# Write your MySQL query statement below
+SELECT *,
+       CASE 
+           WHEN x + y > z AND x + z > y AND y + z > x THEN 'Yes'
            ELSE 'No'
-       END triangle -- 각 세 변의 길이가 다른 두 변의 길이의 합보다 작아야 한다
-FROM TRIANGLE
+       END AS triangle
+FROM Triangle
