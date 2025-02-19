5d5ab8ffba40303baa9b7a62733bbccd86c6ddec
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-02-19 23:43:12 +0900

    Time: 586 ms (92.02%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,8 +1,9 @@
-SELECT DISTINCT l1.num AS ConsecutiveNums -- 중복을 제거하여 L1의 NUM만을 출력
-FROM Logs AS l1,
-     Logs AS l2,
-     Logs AS l3
-WHERE l1.id = l2.id - 1 -- id 를 비교(연속적으로 나타나기 위해서는 각 id - 1이 이전 id와 같아야함)
-  AND l2.id = l3.id - 1
-  AND l1.num = l2.num -- num을 비교한다
+# Write your MySQL query statement below
+SELECT DISTINCT(l1.num) AS ConsecutiveNums
+FROM Logs AS l1
+JOIN Logs AS l2
+  ON l1.id = l2.id - 1
+JOIN Logs AS l3
+  ON l1.id = l3.id - 2
+WHERE l1.num = l2.num
   AND l2.num = l3.num
