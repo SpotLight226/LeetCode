1d35ba93ec5414db1481cb6a61c14cca888878ff
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-02-19 23:50:13 +0900

    Time: 752 ms (73.26%), Space: 0B (100%) - LeetHub

--- 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
@@ -1,9 +1,18 @@
-SELECT P.product_id,
-       COALESCE((SELECT new_price
-                 FROM Products
-                 WHERE product_id = P.product_id
-                   AND change_date <= '2019-08-16'
-                 ORDER BY change_date DESC
-                 LIMIT 1), 10) AS price
-FROM (SELECT DISTINCT(product_id)
-      FROM Products) AS P
+# Write your MySQL query statement below
+-- rank를 이용한 풀이 (기발함)
+-- 제품의 가격 변경 날짜가 2019-08-16 이하인 것 중에서 순위를 매김
+WITH Products_price AS (SELECT *,
+                               RANK() OVER(PARTITION BY product_id ORDER BY change_date DESC) AS price_rank
+                        FROM Products
+                        WHERE change_date <= '2019-08-16')
+-- cte에서 가장 
+SELECT product_id,
+       new_price AS price
+FROM Products_price
+WHERE price_rank = 1
+UNION
+SELECT product_id,
+       10 AS price
+FROM Products
+WHERE product_id NOT IN (SELECT product_id
+                         FROM Products_price)
