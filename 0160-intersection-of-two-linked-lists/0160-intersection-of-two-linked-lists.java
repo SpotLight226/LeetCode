b10abb64a9b295e3f8b81a1e37fe76452d0b5dc9
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-02-20 23:45:30 +0900

    Time: 1 ms (99.93%), Space: 49.3 MB (8.73%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -14,6 +14,10 @@ public class Solution {
         ListNode a = headA;
         ListNode b = headB;
 
+        // A + B 와 B + A 를 비교한다
+        // A + B 와 B + A 는 후반부가 결합된 리스트의 후반부와 동일하다
+        // A + B = 4 -> 1 -> 8 -> 4 -> 5 -> 5 -> 6 -> 1 -> 8 -> 4 -> 5
+        // B + A = 5 -> 6 -> 1 -> 8 -> 4 -> 5 -> 4 -> 1 -> 8 -> 4 -> 5
         while(a != b) {
             if(a == null) {
                 a = headB;
