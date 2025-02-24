14183a6a4516a8feba7d65fb635d82c66e924c10
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-02-24 23:51:25 +0900

    Time: 311 ms (99.36%), Space: 0B (100%) - LeetHub

--- 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
@@ -1,12 +1,7 @@
--- 매니저 아이디가 있는 사람 CTE
-WITH Managers AS (SELECT *
-                  FROM Employees
-                  WHERE manager_id IS NOT NULL) 
-
-SELECT M.employee_id 
-FROM Managers AS M
-LEFT JOIN Employees AS E
-       ON M.manager_id = E.employee_id 
-WHERE E.employee_id IS NULL -- 조인 했을 때, E에서 사원 번호가 NULL이라면 이미 퇴사한 사람
-  AND M.salary < 30000 -- 봉급이 30000보다 낮음
-ORDER BY M.employee_id 
+# Write your MySQL query statement below
+SELECT employee_id
+FROM Employees
+WHERE salary < 30000 -- 연봉이 30000보다 낮은 사람
+AND manager_id NOT IN (SELECT employee_id
+                       FROM Employees)
+ORDER BY employee_id
