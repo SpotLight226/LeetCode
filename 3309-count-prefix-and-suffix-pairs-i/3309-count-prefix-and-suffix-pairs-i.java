5584d30ef030beff717a902373c66e4284b787ad
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-03-21 21:50:08 +0900

    Time: 3 ms (34.76%), Space: 43.3 MB (29.77%) - LeetHub

--- 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.java
+++ 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.java
@@ -1,4 +1,16 @@
 class Solution {
+
+    private boolean check(String str1, String str2) {
+        int s1Len = str1.length();
+        int s2Len = str2.length();
+
+        // s1의 길이가 s2보다 길다면 접두사가 성립 X
+        if(s1Len > s2Len) return false;
+        
+        // s2에서 s1의 길이까지 잘라서 같은지(접두사) && s2에서 s2 - s1 길이부터 끝까지 잘라서 같은지(접미사)
+        return str2.substring(0, s1Len).equals(str1) && str2.substring(s2Len - s1Len).equals(str1);
+    }
+
     public int countPrefixSuffixPairs(String[] words) {
         int len = words.length;
         int result = 0;
@@ -8,11 +20,13 @@ class Solution {
                 String str1 = words[i];
                 String str2 = words[j];
 
-                int s1Len = str1.length();
-                int s2Len = str2.length();
+                // 첫 단어가 같아야 접두사 가능
+                if(str1.charAt(0) != str2.charAt(0)) {
+                    continue;
+                }
 
-                if(!(s1Len > s2Len) && str2.substring(0, s1Len).equals(str1)) {
-                    if(str2.substring(s2Len - s1Len).equals(str1)) result++;
+                if(check(str1, str2)) {
+                    result++;
                 }
             }
         }
