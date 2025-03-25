dcb97c694e67b0585ed712fabefe32ac2ddeba8e
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-03-25 23:32:14 +0900

    Time: 0 ms (100%), Space: 41.5 MB (25.24%) - LeetHub

--- 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.java
+++ 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.java
@@ -3,17 +3,20 @@ class Solution {
         int result = nums[0];
         int sum = nums[0];
 
+        // 요소가 1개라면 그 값을 리턴
         if(nums.length == 1) {
             return result;
         }
 
         for(int i = 1; i < nums.length; i++) {
+            // 다음 요소가 더 크면
             if(nums[i] > nums[i - 1]) {
-                sum += nums[i];
-            } else {
-                sum = nums[i];
+                sum += nums[i]; // 합에 더하기
+            } else { // 작으면
+                sum = nums[i]; // 합계값을 초기화
             }
 
+            // 결과 값을 현재 값과 요소를 더한 값을 비교
             result = Math.max(result, sum);
         }
 
