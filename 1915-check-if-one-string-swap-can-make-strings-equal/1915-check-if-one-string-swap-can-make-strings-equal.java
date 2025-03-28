88750b79c2acb855ffdb4815cc19ced1f9116f84
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-03-28 23:14:51 +0900

    Time: 0 ms (100%), Space: 41.2 MB (96.77%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
@@ -6,22 +6,24 @@ class Solution {
 
         int i = -1;
         int j = -1;
-        int count = 0;
+        int count = 0; // 다른 문자의 개수
 
         for(int k = 0; k < s1.length(); k++) {
+            // 문자가 다르면 
             if(s1.charAt(k) != s2.charAt(k)) {
-                count++;
+                count++; // 개수 + 1
 
+                // 개수가 1 개면, 해당 문자의 인덱스를 i에 저장
                 if(count == 1) {
                     i = k;
-                } else if (count == 2) {
+                } else if (count == 2) { // 개수가 2개면, 위치가 다른 해당 문자의 인덱스를 j에 저장
                     j = k;
                 } else { // 2개 이상 다름
                     return false;
                 }
             } 
         }
-
+        // 2개가 다르며, i,j 인덱스의 문자가 같으면 위치를 변경했을 때, 같은 문자가 됨
         if(count == 2 && s1.charAt(i) == s2.charAt(j) && s1.charAt(j) == s2.charAt(i)) {
             return true;
         }
