a2845d9fc5faf568cc60bbb50dc2b318bf4fa94b
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-03-30 22:25:48 +0900

    Time: 4 ms (92.83%), Space: 42.4 MB (43.2%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.java
+++ 0768-partition-labels/0768-partition-labels.java
@@ -3,16 +3,17 @@ class Solution {
         int[] arr = new int[26];
 
         for(int i = 0; i < s.length(); i++) {
-            arr[s.charAt(i) - 'a'] = i;    
+            arr[s.charAt(i) - 'a'] = i;   // 해당 문자의 마지막 인덱스(위치)
         }
 
         List<Integer> result = new ArrayList<>();
-        int max = 0;
-        int prev = -1;
+        int max = 0; // 가장 긴 문자 길이
+        int prev = -1; // 이전 문자 길이
 
         for(int i = 0; i < s.length(); i++) {
+            // 문자의 마지막 인덱스와 해당 문자에서 잘랐을 시, 문장의 길이
             max = Math.max(max, arr[s.charAt(i) - 'a']);
-
+            
             if(i == max) {
                 result.add(i - prev);
                 prev = i;
