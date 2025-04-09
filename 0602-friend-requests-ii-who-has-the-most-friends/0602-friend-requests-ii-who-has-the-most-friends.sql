8e9c39536af15ade0cb05b8336d8e606efadf50f
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-04-09 21:02:12 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,15 +1,21 @@
--- RequestAccepted는 요청에 대해서 승낙한 id를 나타낸다.
--- 즉, 리퀘스터든 엑셉트든 결국 친구가 되었다는 거임
--- 친구 요청을 승낙한 사람과, 요청한 사람을 각각 나눈 CTE를 만들고 id의 각 개수를 센다 
-WITH IdCte AS (SELECT requester_id AS id
-               FROM RequestAccepted
-               UNION ALL
-               SELECT accepter_id AS id
-               FROM RequestAccepted)
+# Write your MySQL query statement below
+WITH T1 AS (SELECT requester_id AS id,
+                   accepter_id AS acc
+            FROM RequestAccepted),
+
+     T2 AS (SELECT accepter_id AS id
+                   requester_id AS acc
+            FROM RequestAccepted)
 
 SELECT id,
-       COUNT(*) AS num
-FROM IdCte
+       COUNT(DISTINCT acc) AS num
+FROM (SELECT *
+      FROM T1
+
+      UNION
+
+      SELECT *
+      FROM T2) AS Total_Table
 GROUP BY id
-ORDER BY COUNT(*) DESC -- 가장 많은 사람을 고르기 위해 내림차순
-LIMIT 1 -- 가장 위의 1개만 자르기
+ORDER BY num DESC
+LIMIT 1
