d2e72b8b07f45472dc83240e769b4354b6f3f941
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-04-11 21:31:39 +0900

    Time: 585 ms (75.6%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,11 +1,11 @@
+# Write your MySQL query statement below
 SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
 FROM Insurance
 WHERE tiv_2015 IN (SELECT tiv_2015
                    FROM Insurance
                    GROUP BY tiv_2015
-                   HAVING COUNT(*) > 1) -- tiv_2015가 있으며
-AND (lat, lon) IN (SELECT lat,
-                            lon
+                   HAVING COUNT(*) > 1)
+AND (lat, lon) IN (SELECT lat, lon
                    FROM Insurance
                    GROUP BY lat, lon
-                   HAVING COUNT(*) = 1) -- lat, lon 이 고유해야 함
+                   HAVING COUNT(*) = 1)
