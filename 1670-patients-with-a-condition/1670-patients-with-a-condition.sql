523271f34b45fd94200370f265f9815c3326652f
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-04-20 22:50:12 +0900

    Time: 363 ms (83.2%), Space: 0B (100%) - LeetHub

--- 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
@@ -1,4 +1,4 @@
+# Write your MySQL query statement below
 SELECT *
 FROM Patients
-WHERE conditions REGEXP '(^|\\s)DIAB1'
--- (^|\\s) => DIAB1이 문자열의 시작이거나 공백(\s) 바로 다음에 위치해야 함을 의미
+WHERE conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%'
