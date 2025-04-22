e1306e1e849eef98c0ec9451217291dd469444bb
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-04-22 21:13:01 +0900

    Time: 293 ms (66.84%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,5 +1,5 @@
-SELECT MAX(salary) AS SecondHighestSalary 
+# Write your MySQL query statement below
+SELECT max(salary) AS SecondHighestSalary 
 FROM Employee
-WHERE salary < (SELECT MAX(salary) -- 최고 연봉을 조건으로 작은 것 중에서 max 인것
+WHERE salary < (SELECT max(salary)
                 FROM Employee)
-
