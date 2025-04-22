fecb03304abf4f26aa24e8f3fbb4843d8f563c76
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-04-22 21:11:18 +0900

    Time: 1093 ms (47.45%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,19 +1,5 @@
-/* 내 꺼
--- WHERE 조건으로 EMAIL로 그룹화하여 최소 ID(MIN(ID))만을 가진 CTE를 만들고
--- 해당 CTE에서 ID 만을 조건으로 설정하여 ID가 CTE에 포함되지 않은 것만 남기고(NOT IN) 삭제한다
-DELETE FROM Person
-WHERE ID NOT IN (
-    SELECT MIN_ID FROM (
-        SELECT MIN(id) as MIN_ID
-        FROM PERSON
-        GROUP BY EMAIL
-    )
-);
-*/
-
--- PERSON 테이블을 두번 참조하여 P1, P2를 만든다
--- P1.EMAIL = P2.EMAIL 이 조건으로 같은 이메일을 가진 행을 찾는다
--- P1.ID > P2.ID 같은 이메일을 가진 행 중에서 ID가 더 큰 행을 선택하고 P1 행을 삭제한다
-DELETE P1 FROM PERSON AS P1, PERSON AS P2
-WHERE P1.EMAIL = P2.EMAIL AND P1.ID > P2.ID
-
+# Write your MySQL query statement below
+DELETE p1 
+FROM Person p1
+JOIN Person p2
+On p1.email = p2.email AND p1.id > p2.id
