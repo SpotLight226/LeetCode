783586ad811a02707e333bc3ad145df524d130e2
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-04-23 22:00:59 +0900

    Time: 611 ms (51.51%), Space: 0B (100%) - LeetHub

--- 1625-group-sold-products-by-the-date/1625-group-sold-products-by-the-date.sql
+++ 1625-group-sold-products-by-the-date/1625-group-sold-products-by-the-date.sql
@@ -1,7 +1,6 @@
+# Write your MySQL query statement below
 SELECT sell_date,
-       -- 제품의 수를 선택하는 것이므로 중복되는 product는 없이
-       COUNT(DISTINCT(product)) AS num_sold,
-       -- GROUP_CONCAT(여러 요소를 묶어서 하나로 만듦(요소, 정렬 기준) SEPARATOR 구분자)
-       GROUP_CONCAT(DISTINCT(product) ORDER BY product SEPARATOR ',') AS products
-FROM Activities
+       COUNT(DISTINCT product) AS num_sold, -- 중복 제외하고 총 판매개수
+       GROUP_CONCAT(DISTINCT product ORDER BY product) AS products -- GROUP_CONCAT 으로 product의 요소를 묶음(,로 자동구분)
+FROM Activities 
 GROUP BY sell_date
