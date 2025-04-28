a5025bf63bbe023c6e28ec95cef21ede20aed694
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-04-28 13:34:23 +0900

    Time: 57 ms (5.34%), Space: 54.7 MB (5.46%) - LeetHub

--- 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.js
+++ 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.js
@@ -2,16 +2,9 @@
  * @param {...(null|boolean|number|string|Array|Object)} args
  * @return {number}
  */
- /*
 var argumentsLength = function(...args) {
     return args.length;
 };
-*/
-
-// args의 길이만큼을 리턴
-function argumentsLength() {
-    return arguments.length;
-}
 
 /**
  * argumentsLength(1, 2, 3); // 3
