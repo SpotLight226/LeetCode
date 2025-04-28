936729ed773377cd2725d116b5e3ad09b93d3876
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-04-28 13:28:05 +0900

    Time: 47 ms (86.89%), Space: 54.2 MB (91.07%) - LeetHub

--- 2741-function-composition/2741-function-composition.js
+++ 2741-function-composition/2741-function-composition.js
@@ -3,16 +3,15 @@
  * @return {Function}
  */
 var compose = function(functions) {
-	if (functions.length === 0) { // 입력된 functions의 길이가 0이면 그대로 x를 리턴
-    return function(x) { return x; };
-  }
-    // functions에서 오른쪽부터 시작해서 각 function을 실행
-  return functions.reduceRight(function(prevFn, nextFn) {
+    
     return function(x) {
-      return nextFn(prevFn(x)); // 이전 Fn의 값을 다음 Fn에 넣기
-    };
-  });
+        // 오른 쪽에서 왼쪽으로 가니까, 가장 오른쪽의 인덱스부터 시작
+        for(var i = functions.length - 1; i >= 0; i--) {
+            x = functions[i](x);
+        }
 
+        return x;
+    }
 };
 
 /**
