a24d902638ec375292f9a3f2b8969c883634df2e
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-05-02 21:19:19 +0900

    Time: 65 ms (5.5%), Space: 53 MB (95.55%) - LeetHub

--- 2859-add-two-promises/2859-add-two-promises.js
+++ 2859-add-two-promises/2859-add-two-promises.js
@@ -8,10 +8,15 @@ var addTwoPromises = async function(promise1, promise2) {
     const [v1, v2] = await Promise.all([promise1, promise2]);
     return v1 + v2;
     */
+    /*
     // .then 메서드는 Promise가 성공(resolve)되었을 때 실행될 콜백 함수를 등록
     return await Promise.all([promise1, promise2]).then(
         ([v1, v2]) => v1 + v2 //  Promise의 결과값 v1과 v2를 더한 값을 반환
     );
+    */
+
+    const [v1, v2] = await Promise.all([promise1, promise2]);
+    return v1 + v2;
 };
 
 /**
