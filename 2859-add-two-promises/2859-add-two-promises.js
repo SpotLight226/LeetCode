99f8bd92d0e2b12c046d55f617393520f8289d80
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-05-02 21:16:56 +0900

    Time: 51 ms (61.15%), Space: 54.4 MB (27.59%) - LeetHub

--- 2859-add-two-promises/2859-add-two-promises.js
+++ 2859-add-two-promises/2859-add-two-promises.js
@@ -4,9 +4,14 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
+    /*
     const [v1, v2] = await Promise.all([promise1, promise2]);
-
     return v1 + v2;
+    */
+
+    return await Promise.all([promise1, promise2]).then(
+        ([v1, v2]) => v1 + v2
+    );
 };
 
 /**
