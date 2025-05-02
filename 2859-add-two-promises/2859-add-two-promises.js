85a5469b2abf83eb03a3761767211f27f971d8f4
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-05-02 21:15:30 +0900

    Time: 53 ms (49.58%), Space: 54.6 MB (12.72%) - LeetHub

--- 2859-add-two-promises/2859-add-two-promises.js
+++ 2859-add-two-promises/2859-add-two-promises.js
@@ -4,11 +4,9 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
-    // 변수 val1, val2에 await로 2개의 약속 Promise에 all로 모든 변수가 들어올 때까지 대기하다가 
-    // 2개의 변수가 들어오면 val1 + val2를 리턴
-    const [val1, val2] = await Promise.all([promise1, promise2]);
+    const [v1, v2] = await Promise.all([promise1, promise2]);
 
-    return  val1 + val2;
+    return v1 + v2;
 };
 
 /**
