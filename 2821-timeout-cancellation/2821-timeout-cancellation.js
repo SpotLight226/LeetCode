261578a11c3967f832b93ed2b4eb73ed4e13f886
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-05-02 21:27:54 +0900

    Time: 48 ms (84.65%), Space: 54.1 MB (53.26%) - LeetHub

--- 2821-timeout-cancellation/2821-timeout-cancellation.js
+++ 2821-timeout-cancellation/2821-timeout-cancellation.js
@@ -5,8 +5,9 @@
  * @return {Function}
  */
 var cancellable = function(fn, args, t) {
+    // 예약된 timeout변수에다 fn에 변수args를 넣은 것과 시간 t를 저장하고
     var timeout = setTimeout(() => fn(...args), t);
-
+    // 취소 함수인 clearTimeout에 timeout을 넣어서, cancelFn을 실행 시, timeout변수를 초기화한다
     var cancelFn = () => clearTimeout(timeout);
 
     return cancelFn
