750209cd127c60d34d3e3a5b6181674aff2edeea
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-05-06 22:13:46 +0900

    Time: 10 ms (59.8%), Space: 45.6 MB (10.67%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+
+        for(char c : magazine.toCharArray()) {
+            // 해쉬 맵에 c가 없다면 0을 넣고, 있다면 해당 값에 + 1해서 넣기
+            hashMap.put(c, hashMap.getOrDefault(c, 0) + 1);
+        }
+
+        for(char c : ransomNote.toCharArray()) {
+            // magazine의 글자의 갯수로 만든 해쉬 맵에서 ransomNote의 글자가 없거나, 해당 글자의 개수가 0이라면
+            if(!hashMap.containsKey(c) || hashMap.get(c) <= 0) {
+                return false;
+            }
+            // 해쉬 맵을 업데이트
+            hashMap.put(c, hashMap.get(c) - 1);
+        }
+
+        return true;
+    }
+}
