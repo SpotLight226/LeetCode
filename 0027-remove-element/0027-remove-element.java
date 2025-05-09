268ab49089f7ca416f273cfc584f2929259a1f39
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-05-09 23:12:53 +0900

    Time: 0 ms (100%), Space: 41.8 MB (92.56%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,15 +1,14 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        if(nums.length == 0) return 0; // nums 의 길이가 0이라면 0 리턴
-
-        int j = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[j] = nums[i];
-                j++;
+        int k = 0; // 지운 배열의 인덱스 겸, 개수
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
             }
         }
 
-        return j;
+        return k;
     }
 }
