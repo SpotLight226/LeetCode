d45c39643effa64f6c2c57197a73ee343daccb41
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-05-16 20:54:54 +0900

    Time: 2 ms (99.04%), Space: 42.8 MB (89.98%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isPalindrome(String s) {
+        /*
         s = s.toLowerCase().replaceAll("[^a-z0-9]", ""); // s에서 공백, 숫자를 제거
 
         int l = 0;
@@ -14,6 +15,29 @@ class Solution {
             r--;
         }
         // 위의 과정을 거친 후, true 리턴
+        return true;
+        */
+
+        int l = 0;
+        int r = s.length() - 1;
+
+        while(l < r) {
+            char left = s.charAt(l);
+            char right = s.charAt(r);
+
+            if(!Character.isLetterOrDigit(left)) {
+                l++;
+            } else if (!Character.isLetterOrDigit(right)) {
+                r--;
+            } else {
+                if (Character.toLowerCase(left) != Character.toLowerCase(right)) {
+                    return false;
+                }
+                l++;
+                r--;
+            }
+        }
+
         return true;
     }
 }
