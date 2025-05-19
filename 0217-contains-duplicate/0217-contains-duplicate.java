aae6f48032ff303c3f870464bce5374cbd4ccd16
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-05-19 22:57:07 +0900

    Time: 13 ms (65.68%), Space: 59.4 MB (22.04%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        for(int n : nums) {
+            if(hashSet.contains(n)) {
+                return true;
+            }
+            hashSet.add(n);
+        }
+
+        return false;
+    }
+}
