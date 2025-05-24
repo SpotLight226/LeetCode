2542161ead299ac062596f4fc97ad08898ab12f5
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-05-24 23:03:36 +0900

    Time: 1 ms (93.75%), Space: 42.5 MB (30.61%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
@@ -3,7 +3,9 @@ class Solution {
         int result = 0;
 
         for(char c : columnTitle.toCharArray()) {
+            // 해당 문자의 값으로 변환 A = 1(65 - 65 + 1) 이므로, 해당 문자에서 'A'(65) 를 뺀 후, + 1 
             int value = c - 'A' + 1;
+            // Z가 26이니 문자 한 자리 당, 26 => 자릿 수 올림
             result = result * 26 + value;
         }
 
