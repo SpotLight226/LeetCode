c0f12e73ae105a16d068b77095038325c91b3c14
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-06-09 21:59:08 +0900

    Time: 1 ms (87.37%), Space: 40.8 MB (83.44%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,21 +1,25 @@
 class Solution {
     public int mySqrt(int x) {
-       // 이진 탐색을 위한 시작점
+        if(x == 0 || x == 1) return x;
+
+        // 2진 검색
         int l = 1;
         int r = x;
 
-        while (l <= r) {
-            // 이게 가장 헷갈렸는데 int 형은 소수점이 생길 시 반올림된다
-            int mid = l + (r - l) / 2; // 중간값
-            // 중간 값이 x / mid 보다 작다면 ((mid * mid) < x)
-            if (mid < x / mid) {
-                l = mid + 1; // 왼쪽 값을 중간값의 +1
-            } else if (mid > x / mid) { // 중간 값이 x / mid 보다 크다면 ((mid * mid) > x)
-                r = mid - 1; // 오른쪽 값을 중간 값의 -1
+        while(l <= r) {
+            // 중간 값
+            int m = (r - l) / 2 + l;
+
+            // 중간 값이 x를 m으로 나눈 몫보다 크다면 오른쪽을 변경 
+            if(m > x / m) {
+                r = m - 1;
+            } else if (m < x / m) { // 작다면 왼쪽 변경
+                l = m + 1;
             } else {
-                return mid;
+                return m;
             }
         }
-        return r; // 위의 반복문을 거쳐 r이 제곱근으로 리턴 
+
+        return r;
     }
 }
