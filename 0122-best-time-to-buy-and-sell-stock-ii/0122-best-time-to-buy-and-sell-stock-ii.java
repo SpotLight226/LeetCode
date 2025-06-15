e7cf0010e1a5a13ce6f1c549b98966edb6966bf0
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-06-15 22:49:42 +0900

    Time: 1 ms (79.33%), Space: 46 MB (35.95%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,7 +1,8 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int result = 0;
-        
+
+        // 단순하게 다음 날 주식 가격이 더 높다면 판다
         for(int i = 1; i < prices.length; i++) {
             if(prices[i] > prices[i - 1]) {
                 result += prices[i] - prices[i - 1];
