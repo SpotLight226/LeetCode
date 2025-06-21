58c6cacc77b224a4a7815d36493e6b1a042a5db2
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-06-21 22:14:38 +0900

    Time: 0 ms (100%), Space: 42 MB (32.51%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,22 +1,17 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        // 배열을 만들어 마지막 요소의 길이 리턴
-        /* 
-        String[] arr = s.split(" ");
+        int end = s.length() - 1;
 
-        return arr[arr.length - 1].length();
-        */
-
-        // 끝 공백을 제거한 문장에서 가장 마지막 부터 시작해 char가 공백인 곳까지 반복문을 돌려 계산
-        int count = 0;
-        s = s.trim(); // 끝의 공백 제거
-
-        for(int i = s.length() - 1; i >= 0; i--){
-            if(s.charAt(i) == ' ') break;
-
-            count++;
+        while(end >= 0 && s.charAt(end) == ' ') {
+            end--;
         }
 
-        return count;
+        int start = end;
+
+        while(start >= 0 && s.charAt(start) != ' ') {
+            start--;
+        }
+
+        return end - start;
     }
 }
