5522d6d4a04cd8559eb5565f45398163ba6ccdf5
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-06-25 16:37:03 +0900

    Time: 0 ms (100%), Space: 41.8 MB (22.47%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,8 +1,18 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        if (haystack.contains(needle)) {
-            return haystack.indexOf(needle);
+        // needle 의 길이가 더 길면 등장 안함
+        if(haystack.length() < needle.length()) {
+            return -1;
         }
+
+        // haystack 에서 문자열을 잘라냈을 때, needle 의 길이보다 길어야 하므로, haystack.length() - needle.length()
+        // haystack 에서 시작점 i 와 needle 의 길이만큼을 잘랐을 때, needle 과 같다면 시작점 i 리턴
+        for(int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if(haystack.substring(i, i + needle.length()).equals(needle)) {
+                return i;
+            }
+        }
+
         return -1;
     }
 }
