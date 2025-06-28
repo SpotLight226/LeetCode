c510c65ef070ef03034faa1a0c1ce180cafdcffd
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-06-28 19:52:31 +0900

    Time: 15 ms (23.79%), Space: 45.2 MB (38.8%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
+
+        // magazine 의 문자 개수를 넣을 해쉬맵
         HashMap<Character, Integer> hashMap = new HashMap<>();
 
         for(char c : magazine.toCharArray()) {
@@ -7,6 +9,7 @@ class Solution {
             hashMap.put(c, hashMap.getOrDefault(c, 0) + 1);
         }
 
+        // 반복문에서 해쉬맵을 확인하면서 ransomNote 의 글자가 있는지 확인하고, 있다면 개수를 줄여나간다
         for(char c : ransomNote.toCharArray()) {
             // magazine의 글자의 갯수로 만든 해쉬 맵에서 ransomNote의 글자가 없거나, 해당 글자의 개수가 0이라면
             if(!hashMap.containsKey(c) || hashMap.get(c) <= 0) {
