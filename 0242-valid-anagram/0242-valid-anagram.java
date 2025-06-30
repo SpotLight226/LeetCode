744e1156dd5cefec0be68c27fc705867973a86ad
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-06-30 22:59:41 +0900

    Time: 18 ms (15.69%), Space: 44.8 MB (34.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        /*
         // 정렬해서 같아야 anagram
         char[] sArr = s.toCharArray();
         char[] tArr = t.toCharArray();
@@ -8,5 +9,30 @@ class Solution {
         Arrays.sort(tArr);
 
         return Arrays.equals(sArr, tArr);
+        */
+        
+        // hashMap 에 s 의 단어를 key 로 각 개수를 넣는다
+        if(s.length() != t.length()) return false;
+
+        Map<Character, Integer> hashMap = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            hashMap.put(c, hashMap.getOrDefault(c, 0) + 1);
+        }
+
+        // hashMap 에 t 의 단어가 key에 없던가, t 의 단어 개수가 0 이라면 false
+        for(int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+
+            if(!hashMap.containsKey(c) || hashMap.get(c) == 0) {
+                return false;
+            }
+
+            // hashMap 을 갱신
+            hashMap.put(c, hashMap.get(c) - 1);
+        }
+
+        return true;
     }
 }
