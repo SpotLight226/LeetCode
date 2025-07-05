5bbf8d9d256aca49bc801766368372c4a92a2bbc
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-07-05 21:31:23 +0900

    Time: 21 ms (78.83%), Space: 59.9 MB (44.51%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -3,13 +3,15 @@ class Solution {
         Map<Integer, Integer> hashMap = new HashMap<>();
 
         for(int i = 0; i < nums.length; i++) {
-            int n = nums[i];
+            int n = nums[i]; // 배열의 요소
 
+            // 해쉬 맵에 배열의 요소가 있으며(배열 안에 이 요소가 있음)
+            // 현재의 인덱스 - 이전 요소의 인덱스 <= k (k보다 크거나 같을 때)
             if(hashMap.containsKey(n) && i - hashMap.get(n) <= k) {
                 return true;
             }
 
-            hashMap.put(n, i);
+            hashMap.put(n, i); // 해쉬 맵에 배열의 요소와 인덱스를 넣는다
         }
 
         return false;
