a06789f10e0e552f4eed99d805e2337075435570
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-07-06 21:43:51 +0900

    Time: 3 ms (39.51%), Space: 41.5 MB (91.99%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,27 +1,34 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
+        
+        // 결국 이 문제에서는 ([{}]) 의 형태라도,  stack <'(', '[', '{',> 이런 식으로 들어가서,
+        // stack 에 넣으면서, 확인하면 됨
+        for(char c : s.toCharArray()) {
+            // stack 이 비어있지 않을 때,
+            if(!stack.isEmpty()) {
+                // 이전 문자
+                char prev = stack.peek();
 
-        for (Character c : s.toCharArray()) {
-            // c가 (, {, [ 라면 스택에 넣기 
-            if (c == '(' || c == '{' || c == '[') {
-                stack.push(c);
-            } else { // 닫는 괄호라면
-                // 스택이 비어있다면 false리턴
-                if (stack.isEmpty()) {
-                    return false;
-                }
-
-                // 닫는 괄호일 때, 스택의 가장 위에 있는 c가 짝이 맞는 여는 괄호인지 확인
-                char temp = stack.peek();
-                if (c == ')' && temp == '(' || c == '}' && temp == '{' || c == ']' && temp == '[') {
-                    stack.pop();
-                } else {
-                    return false;
+                // 이전 문자와 현재 문자 비교해서 동일하다면
+                if(check(prev, c)) {
+                    stack.pop(); // stack 에서 하나 뺌
+                    continue; 
                 }
             }
+
+            // stack 이 비어있다면, 문자를 넣음
+            stack.push(c);
         }
-        // 모든 과정을 반복했을 때 스택이 비어있어야 true
-        return stack.isEmpty();        
+
+        // 위의 반복이 다 끝났을 때, stack 이 비어있어야 함
+        return stack.isEmpty();
+    }
+    
+    // 이전 문자, 현재 문자 비교
+    private boolean check(char prev, char curr) {
+        return (prev == '(' && curr == ')') ||
+               (prev == '[' && curr == ']') ||
+               (prev == '{' && curr == '}');
     }
 }
