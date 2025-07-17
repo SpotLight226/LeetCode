3f6d3241bf9824c8100894f25064f13c78e8573e
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-07-17 18:02:43 +0900

    Time: 1 ms (99.66%), Space: 55.2 MB (87.48%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,31 +1,23 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] answer = new int[nums.length]; // 리턴 배열
-
-        // 리턴 배열을 1로 초기화 => 배열 생성시 다 0 이므로 곱할 수 잇게 1로
-        for (int i = 0; i < nums.length; i++) {
-            answer[i] = 1;
+        int len = nums.length;
+        int[] result = new int[len];
+        for(int i = 0; i < len; i++) {
+            result[i] = 1;
         }
-        // 왼쪽, 오른쪽 시작 인덱스
-        int l = 1;
-        int r = 1;
 
-        /* 리턴 배열에서 각 인덱스 위치마다 곱해야할 nums의 인덱스
-            [24, 12, 8, 16]
-             1   0   0   0  
-             2   2   1   1
-             3   3   3   2 
-        */
-        for (int i = 0; i < nums.length; i++) {
-            answer[i] *= l;
+        int l = 1;
+        for(int i = 0; i < len; i++) {
+            result[i] *= l;
             l *= nums[i];
         }
 
-        for (int i = nums.length - 1; i >= 0; i--) {
-            answer[i] *= r;
+        int r = 1;
+        for(int i = len - 1; i >= 0; i--) {
+            result[i] *= r;
             r *= nums[i];
         }
 
-        return answer;
+        return result;
     }
 }
