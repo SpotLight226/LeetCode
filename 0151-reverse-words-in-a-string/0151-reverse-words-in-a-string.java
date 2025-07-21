92d32488f5b8b1d895833a070f812a001f78103e
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-07-21 14:53:33 +0900

    Time: 6 ms (85.49%), Space: 43.8 MB (29.58%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,19 +1,31 @@
-import java.util.*;
-
 class Solution {
     public String reverseWords(String s) {
+        // s 를 띄어쓰기(공백)을 기준으로 잘라 배열로
+        String[] arr = s.split(" ");
+        int l = 0;
+        int r = arr.length - 1;
+
+        // l 이 r 보다 커질 때까지(문자열의 중간까지)
+        while(l < r) {
+            // 인덱스 l 의 문장을 임시로 저장해서 위치를 변경
+            String temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            l++;
+            r--;
+        }
+
         StringBuilder sb = new StringBuilder();
-
-        String[] arr = s.split("\\s+"); // 하나 이상 연속된 공백을 포함해 나누어 배열로
-
-        for (int i = arr.length - 1; i >= 0; i--) {
-            sb.append(arr[i]);
-
-            if( i != 0) {
-                sb.append(" ");
+        for(String str : arr) {
+            // 각 문장은 띄어쓰기로 이어져 있음
+            if(!str.isEmpty()) {
+                if(sb.length() > 0) {
+                    sb.append(" ");
+                }
+                sb.append(str);
             }
         }
 
-        return sb.toString().trim(); // 스트링빌더의 불필요한 끝자리 띄어쓰기 제거
+        return sb.toString();
     }
 }
