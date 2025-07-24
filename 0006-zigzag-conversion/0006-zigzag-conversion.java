5b65bd63474a944a7f5363da62709ef204465d40
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-07-24 19:08:18 +0900

    Time: 144 ms (5.13%), Space: 45.9 MB (12.86%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,41 +1,36 @@
 class Solution {
     public String convert(String s, int numRows) {
-        // 줄 수가 1 또는 줄 수가 s의 길이보다 크거나 같다면 
-        // 지그재그를 할 수 없다
-        if(numRows == 1 || numRows >= s.length()) {
-            return s;
-        }
-
+        // 행 수가 1이거나, s의 길이보다 행 수가 크다면, 한 줄로 됨
+        if(numRows == 1 || numRows >= s.length()) return s;
+        
+        // List에 character 을 넣는 배열을 2중으로
         int idx = 0;
         int depth = 1;
-
-        // 지그재그로 만들 2차원 배열
         List<Character>[] rows = new ArrayList[numRows];
         for(int i = 0; i < numRows; i++) {
             rows[i] = new ArrayList<>();
         }
 
         for(char c : s.toCharArray()) {
+            // 각 행에 문자를 넣고
             rows[idx].add(c);
-            
-            // index가 처음인 0 이라면 아래로 내리기 위해 깊이를 1
-            if(idx == 0) {
+            // 다음 depth 로 넘어가기
+            if(idx == 0) { // 첫 줄이면 depth 를 1
                 depth = 1;
-            } else if(idx == numRows - 1) { // 가장 끝이면 위로 올리기 위해 -1
-                depth = - 1;
+            } else if (idx == numRows - 1) { // 가장 끝이면 depth 를 -1
+                depth = -1;
             }
 
             idx += depth;
         }
 
-        // 스트링빌더에 rows의 각 한 줄씩 for문을 돌려서 각 줄의 문자들을 빌더한다
-        StringBuilder answer = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         for(List<Character> row : rows) {
             for(char c : row) {
-                answer.append(c);
+                sb.append(c);
             }
         }
 
-        return answer.toString();
+        return sb.toString();
     }
 }
