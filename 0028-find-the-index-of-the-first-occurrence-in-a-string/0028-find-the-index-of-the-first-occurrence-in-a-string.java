1384a8dbbfc21bce75393cea32a200240ac9ff48
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-07-26 23:23:54 +0900

    Time: 0 ms (100%), Space: 41.9 MB (22%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,18 +1,21 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        // needle 의 길이가 더 길면 등장 안함
-        if(haystack.length() < needle.length()) {
-            return -1;
-        }
+        int hLength = haystack.length();
+        int nLength = needle.length();
 
-        // haystack 에서 문자열을 잘라냈을 때, needle 의 길이보다 길어야 하므로, haystack.length() - needle.length()
-        // haystack 에서 시작점 i 와 needle 의 길이만큼을 잘랐을 때, needle 과 같다면 시작점 i 리턴
-        for(int i = 0; i <= haystack.length() - needle.length(); i++) {
-            if(haystack.substring(i, i + needle.length()).equals(needle)) {
+        // needle 의 길이가 더 길면, haystack 안 에 있을 수가 없음
+        if(hLength < nLength) return -1;
+
+
+        // haystack 에 needle 이 포함되려면 (haystack 의 길이 - needle 의 길이) 의 인덱스 안에서 needle 이 시작되어야 함
+        // ex) sadbutsad(9), sad(3) => 9 - 3 => 6 인덱스 안에 needle 이 시작되어야 함
+        for(int i = 0; i <= hLength - nLength; i++) {
+            // haystack 을 시작점 i 에서 needle 의 길이 만큼을 잘랐을 때, needle 과 같다면 시작점 i 리턴
+            if(haystack.substring(i, i + nLength).equals(needle)) {
                 return i;
             }
         }
-
+        
         return -1;
     }
 }
