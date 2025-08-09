92e5d9d8795b27837fc9c8d7762d0febd7a4b740
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-08-09 20:00:00 +0900

    Time: 33 ms (91.62%), Space: 53.6 MB (34.78%) - LeetHub

--- 2732-counter/2732-counter.js
+++ 2732-counter/2732-counter.js
@@ -3,8 +3,10 @@
  * @return {Function} counter
  */
 var createCounter = function(n) {
+    // 시작 숫자
     let start = n;
 
+    // function 을 호출할 때마다, 시작 숫자에 +1
     return function() {
         return start++;
     };
