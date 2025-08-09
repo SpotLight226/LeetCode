4e4890ed51183b89605b587c5340e3e126b228a3
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-08-09 19:58:24 +0900

    Time: 43 ms (39.77%), Space: 52.4 MB (94.41%) - LeetHub

--- 2809-create-hello-world-function/2809-create-hello-world-function.js
+++ 2809-create-hello-world-function/2809-create-hello-world-function.js
@@ -2,8 +2,10 @@
  * @return {Function}
  */
 var createHelloWorld = function() {
+    // 문장 
     const str = "Hello World";
-
+    
+    // function 실행 시, 문장을 리턴
     return function() {
         return str;
     }
