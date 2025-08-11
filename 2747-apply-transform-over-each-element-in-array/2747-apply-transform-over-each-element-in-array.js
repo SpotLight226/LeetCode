2a6a3c9e4ce56585134a4bf53ff588c20badf74e
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-08-11 09:21:41 +0900

    Time: 36 ms (85.06%), Space: 53.1 MB (77.63%) - LeetHub

--- 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
@@ -4,7 +4,9 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
+    // 리턴할 배열
     const resultArr = [];
+    // 배열의 요소를 index에 따라 하나씩 fn수행
     arr.forEach((element, index) => {
         resultArr[index] = fn(element, index);
     });
