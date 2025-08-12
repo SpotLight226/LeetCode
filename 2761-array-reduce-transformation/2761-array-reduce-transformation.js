a12040705b4ab11d035930cbfe2d61ad3f219621
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-08-12 19:51:46 +0900

    Time: 45 ms (48.54%), Space: 54.3 MB (65.14%) - LeetHub

--- 2761-array-reduce-transformation/2761-array-reduce-transformation.js
+++ 2761-array-reduce-transformation/2761-array-reduce-transformation.js
@@ -5,13 +5,15 @@
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
-    let result = init;
+    let result = init; // 결과에 기본값 init을 설정
 
+    // 반복문으로 결과에 fn 의 결과를 더함
     for(var i = 0; i < nums.length; i++) {
         result = fn(result, nums[i]);
     }
 
     /*
+    // forEach 구문
     nums.forEach(n => {
         result = fn(result, n);
     });
