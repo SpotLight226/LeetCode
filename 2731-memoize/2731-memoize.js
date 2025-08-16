8fff069664864f66fc8b19f8a25c69bfc379e9dd
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-08-16 21:01:44 +0900

    Time: 253 ms (73.83%), Space: 93.1 MB (96.53%) - LeetHub

--- 2731-memoize/2731-memoize.js
+++ 2731-memoize/2731-memoize.js
@@ -3,19 +3,22 @@
  * @return {Function}
  */
 function memoize(fn) {
-    const cache = {};
+    const cache = {}; // 결과를 저장할 객체
 
     return function(...args) {
-        const key = JSON.stringify(args);
+        // ...args : 호출할 때 전달된 인자를 배열로 모음
+        const key = JSON.stringify(args); // // 인자를 문자열로 변환해서 캐시의 key로 사용
 
-        if(key in cache) {
-            return cache[key];
+        // cache 안에 key가 있다면(한 번 호출된 적 있다면)
+        if(key in cache) { 
+            return cache[key]; // cache 의 값을 반환
         }
 
+        //  // fn을 실제로 실행해서 결과를 얻음
         const result = fn.apply(this, args);
-        cache[key] = result;
+        cache[key] = result; // 결과를 캐시에 저장
 
-        return result;
+        return result; 
     }
 }
 
