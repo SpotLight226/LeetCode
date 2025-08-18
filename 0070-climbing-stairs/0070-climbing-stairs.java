12ff045d3cd8d6962abe34eb5b7ec7cf0dc5d345
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-08-18 16:37:38 +0900

    Time: 0 ms (100%), Space: 40.1 MB (97.31%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,18 +1,26 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n == 1 || n == 0) return 1;
-        // 재귀 함수로 1개 올라가는 방식과 2개 올라가는 방식을 호출하여 더하는 방법은 시간 초과
-        // return climbStairs(n -1) + climbStairs(n - 2);
-
-        // dp 배열
-        int[] dp = new int[n + 1];
-        dp[0] = dp[1] = 1; // 처음 과 다음은 1로 고정
-        
-        // i-1, i-2 의 값을 호출하여 마지막 개수를 구한다
-        for (int i = 2; i < n + 1; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+      // 계단이 3단까지는 단수가 오르는 방법
+      // 1) 1
+      // 2) 1 + 1, 2
+      // 3) 1+1, 2+1, 1+2 
+        if (n <= 3) {
+          return n;
         }
 
-        return dp[n];
+      // 계단이 4 이상 이라면, 이전 3단까지 올라가는 방법(3) = prev1
+      // 그 전 단계까지 올라가는 방법(2) = prev2
+        int prev1 = 3;
+        int prev2 = 2;
+        int curr = 0;
+
+        // 현재 방법 + 
+        for(int i = 3; i < n; i++) {
+          curr = prev1 + prev2;
+          prev2 = prev1;
+          prev1 = curr;
+        }
+
+        return curr;
     }
 }
