2656afdd99c589b12833612655acb4ef83f9763a
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-08-23 23:13:37 +0900

    Time: 77 ms (43.1%), Space: 65.4 MB (80.76%) - LeetHub

--- 2804-compact-object/2804-compact-object.js
+++ 2804-compact-object/2804-compact-object.js
@@ -3,14 +3,24 @@
  * @return {Object|Array}
  */
 var compactObject = function(obj) {
-    if(obj === null) return null; // obj가 null이면 null리턴
-    if(Array.isArray(obj)) return obj.filter(Boolean).map(compactObject);  
-    if(typeof obj !== "object") return obj; // obj가 객체(object)가 아니라면 obj리턴
-    
+    if(obj === null) {
+        return null;
+    }
+
+    if (Array.isArray(obj)) {
+        return obj.filter(Boolean).map(compactObject);
+    }
+
+    if (typeof obj !== "object") {
+        return obj;
+    }
+
     const compacted = {};
-    for (const key in obj) {
+    for(const key in obj) {
         let value = compactObject(obj[key]);
-        if (value) compacted[key] = value;
+        if(value) {
+            compacted[key] = value;
+        }
     }
 
     return compacted;
