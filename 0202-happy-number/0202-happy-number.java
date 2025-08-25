1149780c3e0e7b67a511001726d5dff293da1027
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-08-25 22:16:58 +0900

    Time: 1 ms (64%), Space: 40.4 MB (93.32%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,26 +1,36 @@
 class Solution {
     public boolean isHappy(int n) {
-        Set<Integer> set = new HashSet<>();
+        // 투포인터
+        // l = 한 번 계산
+        // r = 두 번 계산
+        int l = getNext(n);
+        int r = getNext(getNext(n));
 
-        while (!set.contains(n)) {
-            set.add(n);
-            n = getNum(n); // 다음 숫자
-            if(n == 1) return true;
+        // 둘이 같다면, 둘 다 1 이거나, 몇 번을 하더라도 1이 될 수 없는 숫자
+        while(l != r) {
+            // 2번 계산한 값이 1이면 바로 true 리턴
+            if(r == 1) {
+                return true;
+            }
+
+            // l, r 값 계산
+            l = getNext(l);
+            r = getNext(getNext(r));
         }
 
-        return false;   
+        return l == 1;
     }
 
-    private int getNum(int n) {
-        int result = 0;
-        
-        // 자리를 하나씩 떼어서 제곱하여 더하기
+    private int getNext(int n) {
+        int next = 0;
+
+        // n 을 한 자리 씩 떼어서 제곱 후 next 에 더하고, n 의 끝자리를 떼어낸 수로 갱신
         while(n > 0) {
             int digit = n % 10;
-            result += digit * digit;
+            next += digit * digit;
             n = n / 10;
         }
 
-        return result;
+        return next;
     }
 }
