4bb2eb5e7e856aa3d73aca4bdee8e06fa4786fc7
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-08-28 19:45:08 +0900

    Time: 47 ms (24.07%), Space: 53.9 MB (54.87%) - LeetHub

--- 2864-is-object-empty/2864-is-object-empty.js
+++ 2864-is-object-empty/2864-is-object-empty.js
@@ -3,12 +3,11 @@
  * @return {boolean}
  */
 var isEmpty = function(obj) {
-    if(typeof obj === 'object' && obj !== null){
-        if(Array.isArray(obj)){
-            return obj.length === 0;
-        }
-        return Object.keys(obj).length === 0;
+    // obj 가 배열이라면
+    if(Array.isArray(obj)) {
+        // obj 의 길이가 0인지 파악해서 리턴
+        return obj.length === 0;
+    } else { // obj 가 배열이 아니고 키 : 값 일 때
+        return Object.keys(obj).length === 0; // Object 에서 key 들의 길이가 0인지 파악해서 리턴
     }
-    
-    return false;
 };
