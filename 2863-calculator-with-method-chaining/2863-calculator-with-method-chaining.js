8ed9d0f99a5c2f2c7e45d5f70a4bcfacf586ff9c
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-08-29 19:35:01 +0900

    Time: 38 ms (73.53%), Space: 53.1 MB (72.39%) - LeetHub

--- 2863-calculator-with-method-chaining/2863-calculator-with-method-chaining.js
+++ 2863-calculator-with-method-chaining/2863-calculator-with-method-chaining.js
@@ -3,63 +3,71 @@ class Calculator {
     /** 
      * @param {number} value
      */
+     // 생성자 => 인스턴스 변수 result 에 value 값을 할당해서 이후 계산에 사용
+     // calculator 의 현재 상태를 객체 내부에 저장하고, 메서드 체이닝을 이어갈 수 있게 인스턴스 변수 사용
     constructor(value) {
-        this.result = value; // 생성 시, result에 입력된 value를 대입
+        this.result = value;
     }
     
     /** 
      * @param {number} value
      * @return {Calculator}
      */
+    // 더하기
     add(value){
         this.result += value;
-        return new Calculator(this.result); // 계산 후, 새로운 Calculator를 생성하여 리턴
+        return this;
     }
     
     /** 
      * @param {number} value
      * @return {Calculator}
      */
+     // 빼기
     subtract(value){
         this.result -= value;
-        return new Calculator(this.result);
+        return this;
     }
     
     /** 
      * @param {number} value
      * @return {Calculator}
      */  
+     // 곱하기
     multiply(value) {
         this.result *= value;
-        return new Calculator(this.result);
+        return this;
     }
     
     /** 
      * @param {number} value
      * @return {Calculator}
      */
+     // 나누기
     divide(value) {
-        if(value === 0) {
-            throw 'Division by zero is not allowed';
-        } else {
-            this.result /= value;
+        // 0으로는 나눌 수 없으므로, 에러를 리턴
+        if (value === 0) {
+            throw new Error('Division by zero is not allowed');
         }
-        return new Calculator(this.result);
+        this.result /= value;
+        return this;
     }
     
     /** 
      * @param {number} value
      * @return {Calculator}
      */
+     // 제곱 => Math 메서드의 pow 를 사용
     power(value) {
         this.result = Math.pow(this.result, value);
-        return new Calculator(this.result);
+        return this;
     }
     
     /** 
      * @return {number}
      */
+     // 결과값을 리턴
     getResult() {
-        return this.result; 
+        return this.result;
     }
 }
