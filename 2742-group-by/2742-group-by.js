4655e82e30e44a4d64eec2000a8f80ecac8b030f
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-08-31 23:08:37 +0900

    Time: 111 ms (29.61%), Space: 79 MB (44.63%) - LeetHub

--- 2742-group-by/2742-group-by.js
+++ 2742-group-by/2742-group-by.js
@@ -3,13 +3,16 @@
  * @return {Object}
  */
 Array.prototype.groupBy = function(fn) {
-    return this.reduce((grouped, item) => {
-        const key = fn(item);
 
+    /// reduce 로 grouped 객체에 요소들을 모은다
+    return this.reduce((grouped, item) => {
+        // fn(item) 의 결과를 key 로 사용
+        const key = fn(item); 
+        // 해당 key 가 없으면 새로운 배열 생성
         if(!grouped[key]) {
             grouped[key] = [];
         }
-
+        // 해당 key 의 배열에 item 추가
         grouped[key].push(item);
 
         return grouped;
