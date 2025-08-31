98c61163cbed618eb5fe9e7ab195f89bdbf5af28
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-08-31 23:10:14 +0900

    Time: 110 ms (56.69%), Space: 70 MB (77.73%) - LeetHub

--- 2860-sort-by/2860-sort-by.js
+++ 2860-sort-by/2860-sort-by.js
@@ -4,8 +4,7 @@
  * @return {Array}
  */
 var sortBy = function(arr, fn) {
-    // 정렬로 기준을 작성해준다
-    // arr 배열에서 비교할 요소를 a와 b로 표기하고 fn에 따라 정의된 a와 b를 비교하여 오름차순으로 비교
-    // 내림차순 비교시, fn(b) - fn(a)
+    // fn(a) - fn(b) → 오름차순 정렬
+    // fn(b) - fn(a) → 내림차순 정렬
     return arr.sort((a, b) => fn(a) - fn(b));
 };
