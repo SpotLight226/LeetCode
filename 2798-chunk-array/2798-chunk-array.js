8606ea009003c28e353a65de4f4e8a44c9d63a3e
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-09-06 22:57:56 +0900

    Time: 48 ms (39.75%), Space: 56.2 MB (48.07%) - LeetHub

--- 2798-chunk-array/2798-chunk-array.js
+++ 2798-chunk-array/2798-chunk-array.js
@@ -5,7 +5,11 @@
  */
 var chunk = function(arr, size) {
     const result = [];
+    // i += size 
+    // ex) size = 2 => slice(0, 2)
+    // i = 2 => slice(2, 4) ...
     for(let i = 0; i < arr.length; i += size) {
+        // arr 을 i 부터 잘라내야할 size 의 크기 까지 잘라서 넣는다
         result.push(arr.slice(i, i + size));
     }
 
