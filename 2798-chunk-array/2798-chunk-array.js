70eccae003929f0c0532172e845e1e08447a330f
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-09-06 22:56:00 +0900

    Time: 29 ms (99.28%), Space: 57.4 MB (21.32%) - LeetHub

--- 2798-chunk-array/2798-chunk-array.js
+++ 2798-chunk-array/2798-chunk-array.js
@@ -4,12 +4,10 @@
  * @return {Array}
  */
 var chunk = function(arr, size) {
-    let chunkArr = [];
-
-    for(let i = 0; i < arr.length; i += size){
-        // arr 을 잘라서 chunk 에 넣기
-        chunkArr.push(arr.slice(i, i + size));
+    const result = [];
+    for(let i = 0; i < arr.length; i += size) {
+        result.push(arr.slice(i, i + size));
     }
 
-    return chunkArr;
+    return result;
 };
