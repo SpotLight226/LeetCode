94fd3d1c49b78cef415bed5b2f88fea9b01cda43
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-09-06 22:58:55 +0900

    Time: 42 ms (45.87%), Space: 53.9 MB (12.11%) - LeetHub

--- 2734-array-prototype-last/2734-array-prototype-last.js
+++ 2734-array-prototype-last/2734-array-prototype-last.js
@@ -2,10 +2,10 @@
  * @return {null|boolean|number|string|Array|Object}
  */
 Array.prototype.last = function() {
-    if(this.length == 0){ // 길이가 0이면 -1
+    if(this.length === 0) {
         return -1;
-    } else{
-        return this[this.length - 1]; // 아니면 가장 끝 요소 리턴
+    } else {
+        return this[this.length - 1];
     }
 };
 
