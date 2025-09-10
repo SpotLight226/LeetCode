58426bfdc9bffdd8080347776d503b3bade8569d
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-09-10 19:58:19 +0900

    Time: 0 ms (100%), Space: 43.3 MB (28.51%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,22 +1,23 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        // 2진 검색을 위한 인덱스
         int l = 0;
         int r = nums.length - 1;
 
-        while (l <= r) {
-            int mid = (l + r) / 2; // 중간 값
-            // 중간 인덱스의 값을 기준으로 l(왼쪽), r(오른쪽) 의 값을 조정한다
-            // target을 찾았으면 리턴
-            if (nums[mid] == target) {
+        while(l <= r) {
+            // 중간 인덱스
+            int mid = l + (r - l) / 2;
+
+            // 찾았으면 mid 인덱스 리턴
+            if(nums[mid] == target) {
                 return mid;
-            } else if (nums[mid] < target) { // mid인덱스의 값이 target보다 작다면 l을 mid + 1로
-                l = mid + 1;
-            } else {
-                r = mid - 1; // mid인덱스 값이 target보다 크다면 r을 mid - 1로
+            } else if (nums[mid] > target) { // target 이 mid 인덱스 값보다 크다면, 
+                r = mid - 1; // 오른쪽 인덱스를 mid 인덱스 앞으로
+            } else { // 작다면
+                l = mid + 1; // 왼쪽 인덱스를 mid 인덱스 뒤로
             }
         }
 
+        // 모든 반복을 진행 후에 target을 못 찾았다면, 넣을 곳인 왼쪽 인덱스를 리턴
         return l;
     }
 }
