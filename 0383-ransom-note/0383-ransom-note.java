d412c61578f719da8115bb334fae60e4ad4eef82
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-09-10 19:54:41 +0900

    Time: 10 ms (50.87%), Space: 45.3 MB (25.24%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,21 +1,19 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-
-        // magazine 의 문자 개수를 넣을 해쉬맵
         HashMap<Character, Integer> hashMap = new HashMap<>();
 
+        // magazine 의 단어를 해쉬 맵에 단어를 key, 개수를 value 로 넣는다
         for(char c : magazine.toCharArray()) {
-            // 해쉬 맵에 c가 없다면 0을 넣고, 있다면 해당 값에 + 1해서 넣기
             hashMap.put(c, hashMap.getOrDefault(c, 0) + 1);
         }
 
-        // 반복문에서 해쉬맵을 확인하면서 ransomNote 의 글자가 있는지 확인하고, 있다면 개수를 줄여나간다
         for(char c : ransomNote.toCharArray()) {
-            // magazine의 글자의 갯수로 만든 해쉬 맵에서 ransomNote의 글자가 없거나, 해당 글자의 개수가 0이라면
+            // 해쉬 맵에 ransomeNote 의 단어가 없거나, 해당 단어의 개수가 이미 사용되어 0 or -1 이라면 false 리턴
             if(!hashMap.containsKey(c) || hashMap.get(c) <= 0) {
                 return false;
             }
-            // 해쉬 맵을 업데이트
+
+            // ransomNote 의 단어로 사용 
             hashMap.put(c, hashMap.get(c) - 1);
         }
 
