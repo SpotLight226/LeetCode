1b356abfeb11204ce58f92608a90e374448bdc1b
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-09-11 17:42:17 +0900

    Time: 2 ms (72.29%), Space: 61.6 MB (75.46%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,17 +1,18 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int buy = prices[0]; // 기본 구매가격은 일단 첫 요소
-        int result = 0;
+        // 처음 구매 가격은 첫번째 값으로
+        int buy = prices[0]; 
+        int profit = 0;
 
-        for(int i = 0; i < prices.length; i++) {
-            // 다른 가격이 구매 가격보다 작다면 산 가격을 변경
-            if(prices[i] < buy) {
+        for(int i = 1; i < prices.length; i++) {
+            // 구매 가격이 다른 값보다 크다면, 구매 가격 교체
+            if(buy > prices[i]) {
                 buy = prices[i];
-            } else if (prices[i] - buy > result) { // 판매 가격 - 구매 가격을 계산해서, 이익이 큰지 확인
-                result = prices[i] - buy; // 이익 변경
             }
+            // 이득 => 기존 이득과 다음 이득을 비교해서 교체
+            profit = Math.max(profit, prices[i] - buy);
         }
 
-        return result;
+        return profit;
     }
 }
