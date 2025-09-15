5dc44cf2c7b4ad72aeea9613c4debb2e1c9bad67
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-09-15 22:28:15 +0900

    Time: 0 ms (100%), Space: 42.6 MB (8.5%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,20 +1,15 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int mid = m -1; // nums1의 마지막 값 위치
-        int left = n - 1; // nums2의 마지막 값 위치
-        int right = m + n - 1; // 최종 병합된 배열의 길이
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
 
-        while(left >= 0) {
-            // num1의 값과 num2의 값을 비교
-            if(mid >= 0 && nums1[mid] > nums2[left]) { // nums1의 값이 클 때
-                nums1[right] = nums1[mid]; // nums1의 끝에 배치
-                mid--; // num1의 값을 배치 했으므로, -1
-            } else { // num2의 값이 클 때,
-                nums1[right] = nums2[left]; // num2의 값을 끝에 배치
-                left--; // num2의 값을 배치 했으므로, -1
+        while(j >= 0) {
+            if(i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
             }
-
-            right--; // 한 값을 배치 했으므로, 다음 마지막 배치 위치를 -1
         }
     }
 }
