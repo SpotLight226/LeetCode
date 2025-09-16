c2a87a94bb8d45bcb6c7079c8f2c2f594a19a512
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-09-16 23:18:54 +0900

    Time: 1 ms (59.36%), Space: 44.8 MB (80.74%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -4,15 +4,15 @@ class Solution {
             return 0;
         }
 
-        int k = 1;
+        int i = 1;
 
-        // 
-        for(int i = 1; i < nums.length; i++) {
-            if(nums[i] != nums[k - 1]) {
-                nums[k] = nums[i];
-                k++;
+        for(int j = 1; j < nums.length; j++) {
+            if(nums[j] != nums[i - 1]) {
+                nums[i] = nums[j];
+                i++;
             }
         }
-        return k;
+
+        return i;
     }
 }
