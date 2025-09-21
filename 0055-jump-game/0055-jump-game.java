fcde67713993ed4a855d743f62d975a75f4b58ef
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-09-21 21:56:46 +0900

    Time: 1 ms (99.91%), Space: 46.2 MB (8%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -4,6 +4,7 @@ class Solution {
         int last = nums.length - 1; 
 
         for(int i = nums.length - 2; i >= 0; i--) {
+            // 현재 위치 + 최대 점프 길이 >= 가야할 길이
             if(i + nums[i] >= last) {
                 last = i;
             }
