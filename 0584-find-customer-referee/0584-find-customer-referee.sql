19edd4cc3f60d04bca7ef944d905bf9325c9d7bc
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-09-22 23:24:49 +0900

    Time: 514 ms (27.61%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,10 +1,6 @@
-/*
-SELECT NAME
-FROM CUSTOMER
-WHERE REFEREE_ID != 2 -- id가 2인 손님이 추천하지 않았고
-  OR REFEREE_ID IS NULL -- 추천 id가 NUll
-*/
-
+# Write your MySQL query statement below
 SELECT name
 FROM Customer
-WHERE COALESCE(referee_id, 0) <> 2; -- referee_id가 null일 때, 0으로 바꾸고 2와 다른 것(<>)을 찾는다
+WHERE referee_id != 2
+   OR referee_id is null
+-- referee_id 가 2 가 아니거나, 아무도 추천하지 않은 name 만
