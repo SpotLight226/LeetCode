647f8534287aab00aadac62d9915dac3759b33b0
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-09-22 23:28:03 +0900

    Time: 485 ms (93.75%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -5,7 +5,9 @@ WHERE tiv_2015 IN (SELECT tiv_2015
                    FROM Insurance
                    GROUP BY tiv_2015
                    HAVING COUNT(*) > 1)
-AND (lat, lon) IN (SELECT lat, lon
+AND (lat, lon) IN (SELECT lat,
+                          lon
                    FROM Insurance
-                   GROUP BY lat, lon
+                   GROUP BY lat, 
+                            lon
                    HAVING COUNT(*) = 1)
