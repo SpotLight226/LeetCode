30be398abfb3a8e5b0cf0b9c905f4c7011be5304
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-09-24 18:13:38 +0900

    Time: 512 ms (25.68%), Space: 0B (100%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -1,9 +1,11 @@
-SELECT DISTINCT(T1.id),
-       CASE
-           WHEN T1.p_id IS NULL THEN 'Root'
-           WHEN T2.id IS NULL THEN 'Leaf'
-           ELSE 'Inner'
+# Write your MySQL query statement below
+SELECT id,
+       CASE 
+           WHEN p_id IS NULL THEN 'Root'
+           WHEN id IN (SELECT p_id FROM Tree)THEN 'Inner'
+           ELSE 'Leaf'
        END AS type
-FROM Tree AS T1
-LEFT JOIN Tree AS T2
-  ON T1.id = T2.P_id
+ FROM Tree
+ -- p_id 가 null 이면 root 
+ -- id 가 p_id 중에서 있다면 Inner
+ -- 아무것도 아니라면 leaf
