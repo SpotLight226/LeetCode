1d934d5a158a0f64aea613d1c4947d14f825a276
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-09-27 22:22:20 +0900

    Time: 247 ms (45.03%), Space: 0B (100%) - LeetHub

--- 0627-swap-salary/0627-swap-salary.sql
+++ 0627-swap-salary/0627-swap-salary.sql
@@ -1,5 +1,7 @@
-UPDATE Salary
-SET sex = CASE
-              WHEN sex = 'f' THEN 'm'
-              ELSE 'f'
-          END
+# Write your MySQL query statement below
+UPDATE salary
+   SET sex = CASE
+                 WHEN sex = 'm' THEN 'f'
+                 ELSE 'm'
+             END
+-- sex 를 CASE WHEN 문으로 m 이면 f로 변경, m 이 아닌 경우, m 으로 변경
