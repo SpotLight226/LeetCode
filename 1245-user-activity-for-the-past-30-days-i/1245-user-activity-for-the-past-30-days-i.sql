a88ae4d4c97e561c8e47e9271ccb4cded7e51e1f
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-10-01 19:22:11 +0900

    Time: 424 ms (96.54%), Space: 0B (100%) - LeetHub

--- 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
@@ -1,7 +1,6 @@
 # Write your MySQL query statement below
-SELECT activity_date AS day, -- 날짜
-       COUNT(DISTINCT(user_id)) AS active_users -- 각 날짜에 유저의 수 (중복 제외)
+SELECT activity_date AS day,
+       COUNT(DISTINCT user_id) AS active_users
 FROM Activity
--- 조건 : 활동 날짜가 6-27 부터 7-27 까지
-WHERE (activity_date > "2019-06-27" AND activity_date <= "2019-07-27") 
+WHERE activity_date <= '2019-07-27' and datediff('2019-07-27', activity_date) < 30
 GROUP BY activity_date
