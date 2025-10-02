d32e61f86feaae5eb3dfce1ef6dee4028b47f736
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-10-02 18:12:43 +0900

    Time: 1026 ms (12.29%), Space: 0B (100%) - LeetHub

--- 1734-bank-account-summary-ii/1734-bank-account-summary-ii.sql
+++ 1734-bank-account-summary-ii/1734-bank-account-summary-ii.sql
@@ -1,7 +1,12 @@
+WITH Balance AS (SELECT account,
+                        SUM(amount) AS balance
+                 FROM Transactions
+                 GROUP BY account
+                 HAVING SUM(amount) > 10000)
+-- Transactions 테이블에서 amount 의 합이 10000보다 높은 것만 뽑은 임시 테이블
+
 SELECT u.name,
-       SUM(amount) AS balance
+       b.balance
 FROM Users AS u
-JOIN Transactions AS t
-  ON u.account = t.account
-GROUP BY u.name
-HAVING SUM(amount) > 10000;
+JOIN Balance AS b
+  ON u.account = b.account
