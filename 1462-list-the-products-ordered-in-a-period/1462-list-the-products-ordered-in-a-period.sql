e4092b6f647b818b09bcd92410dc654c70c70962
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-10-02 18:04:35 +0900

    Time: 661 ms (79.42%), Space: 0B (100%) - LeetHub

--- 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
@@ -1,13 +1,14 @@
 # Write your MySQL query statement below
-WITH feb_Order AS (SELECT product_id,
-                          SUM(unit) AS unit
-                   FROM Orders
-                   WHERE order_date LIKE '2020-02-%'
-                   GROUP BY product_id
-                   HAVING SUM(unit) >= 100)
+WITH Feb_orders AS (SELECT product_id,
+                           SUM(unit) AS unit
+                    FROM Orders
+                    WHERE order_date >= '2020-02-01' AND order_date < '2020-03-01'
+                    GROUP BY product_id
+                    HAVING unit >= 100)
+-- Orders 테이블에서 2020년 2월 총 주문 100 이상인 것 만을 뽑은 임시 테이블
 
-SELECT product_name,
-       unit
+SELECT p.product_name,
+       f.unit
 FROM Products AS p
-JOIN feb_Order AS f
+JOIN Feb_orders AS f
   ON p.product_id = f.product_id
