1325d4a75269449b42484b5c88e84b7f49f1a821
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-10-07 23:04:14 +0900

    Time: 305 ms (35.42%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,6 +1,7 @@
+# Write your MySQL query statement below
 SELECT name,
        population,
        area
 FROM World
-WHERE population >= 25000000 -- 인구가 250만 이상
-   OR area >= 3000000 -- 면적이 300만 이상
+WHERE area >= 3000000
+   OR population >= 25000000
