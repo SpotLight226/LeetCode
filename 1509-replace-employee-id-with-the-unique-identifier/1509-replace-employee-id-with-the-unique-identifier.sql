9d29165cb7bc15fdd19b9554367127907d5ff4d0
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-10-09 18:55:21 +0900

    Time: 1056 ms (85.28%), Space: 0B (100%) - LeetHub

--- 1509-replace-employee-id-with-the-unique-identifier/1509-replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/1509-replace-employee-id-with-the-unique-identifier.sql
@@ -1,5 +1,6 @@
-SELECT EU.unique_id, -- unique_id가 null이면 null로 표기
-       E.name
-FROM Employees AS E
-LEFT JOIN EmployeeUNI AS EU -- Left Join으로 외래키인 id가 null이어도 표기함
-       ON E.id = EU.id
+# Write your MySQL query statement below
+SELECT u.unique_id,
+       e.name
+FROM Employees AS e
+LEFT JOIN EmployeeUNI AS u
+  ON e.id = u.id;
