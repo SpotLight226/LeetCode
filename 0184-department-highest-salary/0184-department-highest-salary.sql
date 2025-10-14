9559a198f1fc321891bb60a6f425c14d3a25fbc0
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-10-14 23:08:02 +0900

    Time: 569 ms (93.01%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+/*
 SELECT d.name AS Department,
        e.name AS Employee,
        e.salary AS Salary
@@ -10,3 +11,19 @@ WHERE e.salary = (SELECT MAX(salary)
                   WHERE departmentId = e.departmentId)
 -- 부서 이름을 쓰기위해 Employee 테이블과 Department 테이블을 조인하고
 -- salary 에서 조건을 departmentId 가 같은 것들 중에서 최고 값을 가진 것들만 선택
+*/
+
+WITH max_salary AS (SELECT departmentId,
+                           MAX(salary) AS highest
+                    FROM Employee
+                    GROUP BY departmentId)
+
+SELECT d.name AS Department,
+       e.name AS Employee,
+       e.salary AS Salary
+FROM Employee AS e
+JOIN max_salary AS m
+  ON e.departmentId = m.departmentId
+JOIN Department AS d
+  ON e.departmentId = d.id
+WHERE e.salary = m.highest
