d3ad672f4684641a8716630793869e4dbd5f7a03
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-10-14 22:51:35 +0900

    Time: 1700 ms (5%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
-SELECT email
-FROM person
+SELECT p1.email
+FROM Person AS p1
 GROUP BY email
-HAVING COUNT(EMAIL) > 1
+HAVING COUNT(email) > 1
+-- person 테이블에서 email 로 그룹화해서 email 의 개수가 1 보다 많은 것만
