71e9bf61777726a9b8878ae584e28694295196a9
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-10-14 22:53:37 +0900

    Time: 338 ms (82.24%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,6 +1,13 @@
 # Write your MySQL query statement below
+/*
 SELECT p1.email
 FROM Person AS p1
 GROUP BY email
 HAVING COUNT(email) > 1
 -- person 테이블에서 email 로 그룹화해서 email 의 개수가 1 보다 많은 것만
+*/
+
+SELECT DISTINCT(p1.email)
+FROM Person AS p1, Person AS p2
+WHERE p1.id <> p2.id AND p1.email = p2.email
+-- Person 테이블을 2개 써서 id가 다른데 email 은 같은 것에서 중복 제거하고
