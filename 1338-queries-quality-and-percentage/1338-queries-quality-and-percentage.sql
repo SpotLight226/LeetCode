1b0c4bd6091c84d2ec0d2a0f34d15b44edbc34a2
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-10-27 23:07:59 +0900

    Time: 2224 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
@@ -1,6 +1,8 @@
+# Write your MySQL query statement below
 SELECT query_name,
-       ROUND(AVG(rating / position), 2) AS quality, -- rating / position 의 비율 구하고 둘 째자리에서 반올림
-       ROUND(AVG(IF(rating < 3, 1, 0)) * 100, 2) AS poor_query_percentage
+       ROUND(SUM(rating / position) / COUNT(query_name), 2) AS quality,
+       ROUND(SUM(IF (rating < 3, 1, 0)) * 100.0 / COUNT(query_name), 2) AS poor_query_percentage
 FROM Queries
-WHERE query_name IS NOT NULL
 GROUP BY query_name
+-- quality 는 (rating / position) 을 다 합친 것 / 해당 query 의 개수 
+-- poor_query 는 rating 이 3 미만 인 것만 합친 것 * 100.0 / query의 개수
