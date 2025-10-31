f80e1ec03809a7429a60ad01bb5af454dc86ebfa
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-10-31 18:41:29 +0900

    Time: 971 ms (43.89%), Space: 0B (100%) - LeetHub

--- 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
@@ -1,15 +1,7 @@
-WITH TotalUsers AS (SELECT COUNT(*) AS total_users
-                    FROM Users), -- 전체 유저 수
-                    
-ContestRegistrations AS (SELECT R.contest_id,
-                         COUNT(*) AS num_users_registered
-                         FROM Register AS R
-                         JOIN Users AS U
-                           ON R.user_id = U.user_id
-                         GROUP BY R.contest_id) -- 각 콘테스트 당 유저의 아이디 수
-                         
-SELECT CR.contest_id,
-       ROUND((CR.num_users_registered / TU.total_users * 100), 2) AS percentage
-FROM ContestRegistrations AS CR
-CROSS JOIN TotalUsers AS TU
-ORDER BY percentage DESC, CR.contest_id ASC;
+# Write your MySQL query statement below
+SELECT contest_id,
+       ROUND(COUNT(DISTINCT user_id) * 100 / (SELECT COUNT(user_id) FROM Users), 2) AS percentage
+FROM Register
+GROUP BY contest_id
+ORDER BY percentage DESC, contest_id
+-- Register 에서 contest_id 로 그룹화하고 (중복되지 않은 신청자의 id * 100) / (User 테이블에서 user의 개수)로 퍼센트를 구한다
