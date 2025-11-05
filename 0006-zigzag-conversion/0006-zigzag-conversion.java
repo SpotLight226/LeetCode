9748671f2b3f445d8fe3a1852d9ed5a2c8c32fa9
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-11-05 17:29:02 +0900

    Time: 6 ms (28.02%), Space: 47.2 MB (5.62%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,30 +1,28 @@
 class Solution {
     public String convert(String s, int numRows) {
-        // 행 수가 1이거나, s의 길이보다 행 수가 크다면, 한 줄로 됨
-        if(numRows == 1 || numRows >= s.length()) return s;
-        
-        // List에 character 을 넣는 배열을 2중으로
-        int idx = 0;
-        int depth = 1;
+        if(numRows == 1 || numRows >= s.length()) {
+            return s;
+        }
+        StringBuilder sb = new StringBuilder();
+
+        int idx = 0; 
+        int d = 1;
+
         List<Character>[] rows = new ArrayList[numRows];
         for(int i = 0; i < numRows; i++) {
             rows[i] = new ArrayList<>();
         }
 
         for(char c : s.toCharArray()) {
-            // 각 행에 문자를 넣고
             rows[idx].add(c);
-            // 다음 depth 로 넘어가기
-            if(idx == 0) { // 첫 줄이면 depth 를 1
-                depth = 1;
-            } else if (idx == numRows - 1) { // 가장 끝이면 depth 를 -1
-                depth = -1;
+            if(idx == 0) {
+                d = 1;
+            } else if (idx == numRows - 1) {
+                d = -1;
             }
-
-            idx += depth;
+            idx += d;
         }
 
-        StringBuilder sb = new StringBuilder();
         for(List<Character> row : rows) {
             for(char c : row) {
                 sb.append(c);
@@ -32,5 +30,6 @@ class Solution {
         }
 
         return sb.toString();
+
     }
 }
