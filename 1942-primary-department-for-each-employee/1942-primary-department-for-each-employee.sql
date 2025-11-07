af6d764ece7e1fbe2f4fa3127d3d200e8c1dfe81
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-11-07 21:17:08 +0900

    Time: 584 ms (21.78%), Space: 0B (100%) - LeetHub

--- 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -2,8 +2,8 @@
 SELECT employee_id,
        department_id
 FROM Employee
-WHERE primary_flag = 'Y' -- 주 부서 이거나
-   OR employee_id in (SELECT employee_id
-                      FROM Employee
-                      GROUP BY employee_id
-                      HAVING COUNT(employee_id) = 1) -- id가 하나 뿐인 서브쿼리에 속해있는 것만
+WHERE primary_flag = 'Y' OR employee_id IN (SELECT employee_id
+                                            FROM Employee
+                                            GROUP BY employee_id
+                                            HAVING COUNT(employee_id) = 1)
+-- primary_flag 가 Y 거나 employee_id 가 하나 뿐인 데이터만을 선택
