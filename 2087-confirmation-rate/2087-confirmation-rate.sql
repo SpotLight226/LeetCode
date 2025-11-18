7dbb8a49927222ce587d1e27167e2a6739b92947
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-11-18 21:54:24 +0900

    Time: 612 ms (61.65%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -1,22 +1,9 @@
-/*
--- 내가 한 것 88비트
--- 조건을 많이 적용해야해서 좀 헷갈렸음
--- 만약 Confirmations테이블에서 user_id가 0개라면 0, 아니라면 action이 컨펌인 것만 1로 해서 더한 것(아니라면 0)을
--- Confirmations테이블의 user_id 개수로 나눈 것을 2번째 자리까지 반올림한 것을 rate로 나타낸다
-SELECT S.user_id,
-       IF(COUNT(C.user_id) = 0, 0, ROUND(SUM(IF(action = 'confirmed', 1, 0)) / COUNT(C.user_id), 2)) AS confirmation_rate
-FROM Signups AS S
-LEFT JOIN Confirmations AS C
-       ON S.user_id = C.user_id
-GROUP BY S.user_id
-ORDER BY confirmation_rate
-*/
-
--- 대박! 96비트
--- AVG에 IF문을 적용해서 action이 컨펌인 것만 1, 아닌것은 0으로 해서 각 아이디의 개수에서 평균을 구한다
-SELECT S.user_id,
-       ROUND(AVG(IF(C.action = 'confirmed', 1, 0)), 2) AS confirmation_rate
-FROM Signups AS S
-LEFT JOIN Confirmations AS C
-       ON S.user_id = C.user_id
-GROUP BY S.user_id
+# Write your MySQL query statement below
+SELECT s.user_id,
+       ROUND(AVG(If(c.action = 'confirmed', 1.00, 0)), 2) AS confirmation_rate
+FROM Signups AS s
+LEFT JOIN Confirmations AS c
+       ON s.user_id = c.user_id
+GROUP BY s.user_id
+-- Signups 테이블과 Confirmations 테이블을 조인하고 user_id 로 그룹화하여
+-- action 이 confirmed 인 것만 선택하여 1.00으로 취급하여 평균을 낸다(소수점 2자리까지 반올림)
