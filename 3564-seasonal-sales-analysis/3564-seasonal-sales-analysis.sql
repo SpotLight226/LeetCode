bcd954458ee4d4eb1a1e01bb80c3520727347f5a
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2025-11-24 21:06:35 +0900

    Time: 521 ms (43.59%), Space: 0B (100%) - LeetHub

---
+++ 3564-seasonal-sales-analysis/3564-seasonal-sales-analysis.sql
@@ -0,0 +1,22 @@
+WITH seasons AS(SELECT CASE
+                          WHEN MONTH(s.sale_date) IN (12,1,2) THEN 'Winter'
+                          WHEN MONTH(s.sale_date) IN (3,4,5) THEN 'Spring'
+                          WHEN MONTH(s.sale_date) IN (6,7,8) THEN 'Summer'
+                      ELSE 'Fall' END AS season,
+                      p.category,
+                      SUM(s.quantity) AS total_quantity,
+                      SUM(s.quantity * s.price) AS total_revenue
+               FROM sales AS s
+               JOIN products AS p
+                 ON s.product_id = p.product_id 
+               GROUP BY season,
+                        p.category),
+     ranks AS (SELECT *,
+                      ROW_NUMBER() OVER (PARTITION BY season ORDER BY total_quantity DESC, total_revenue DESC ) AS rn
+               FROM seasons)
+SELECT season,
+       category,
+       total_quantity,
+       total_revenue
+FROM ranks
+WHERE rn = 1;
